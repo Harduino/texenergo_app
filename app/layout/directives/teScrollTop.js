9cf225300149ede371b04cc57f4f20e3e243c7a9
Egor Lobanov <trickster13r@yandex.ru>
2016-02-11 00:40:40 +0300

    animation for scroll up button

--- app/layout/directives/teScrollTop.js
+++ app/layout/directives/teScrollTop.js
@@ -3,7 +3,7 @@
  * Append button to scroll top
  */
 (function(){
-    angular.module('app.layout').directive('teScrollTop', function(){
+    angular.module('app.layout').directive('teScrollTop', ['$window', function($window){
         return {
             restrict: 'E',
             link: function(scope, element){
@@ -11,10 +11,26 @@
                 scope.teScrollTop = function(){
                     angular.element('body').scrollTop(0);
                 };
+
+                var w = $(window);
+
+                w.scroll(onScroll);
+
+                function onScroll() {
+                    var top = w.scrollTop(),
+                        height = w.height();
+                    if(top>=height*2){
+                        !element.hasClass('slideUp') && element.addClass('slideUp');
+                    }else element.removeClass('slideUp');
+                }
+
+                scope.$on('$destroy', function(){
+                   w.off('scroll', onScroll);
+                });
             },
             template: '<div ng-click="teScrollTop()">'+
                 '<span class="glyphicon glyphicon-arrow-up"></span>'+
             '</div>'
         };
-    });
+    }]);
 }());
