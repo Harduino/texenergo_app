eecaeb43108f4a8fe8ff3e92bfaa211d0b4d7309
Egor Lobanov <egorlobanov@MacBook-Pro-Egor.local>
2016-03-01 01:35:35 +0300

    template for dashboard logs table

--- app/dashboard/controllers/DashboardCtrl.js
+++ app/dashboard/controllers/DashboardCtrl.js
@@ -3,20 +3,40 @@
     'use strict';
 
     angular.module('app.dashboard').controller('DashboardCtrl', ['$scope', 'serverApi', function ($scope, serverApi) {
-        $scope.data = {
+        var sc = $scope;
+        sc.data = {
             customerOrders:[],
-            dispatchOrders:[]
+            dispatchOrders:[],
+            parseLog: [],
+            searchLog: [],
+            parseLogSearchQ: "",
+            searchLogSearchQ: ""
         };
+
+
         
         serverApi.getCustomerOrders(1, "", {}, function(result){
-            console.log('customer_orders', result);
-            $scope.data.customerOrders = result.data.slice(0,10);
+            sc.data.customerOrders = result.data.slice(0,10);
         });
 
         serverApi.getDispatchOrders(1, "", {}, function(result){
-            $scope.data.dispatchOrders = result.data.slice(0,10);
+            sc.data.dispatchOrders = result.data.slice(0,10);
         });
-        
+
+//        serverApi.getSearchLogs(1, "", {}, function(result){
+//           console.log(result.data.length);
+//        });
+
+        sc.filterLogTable = function(e, type){
+            if(e.keyCode == 13){
+                // filter table depending on type
+            }
+        };
+
+        sc.refreshTable = function(type){
+            //cal refresh functions
+        }
+
     }]);
 
 }());
