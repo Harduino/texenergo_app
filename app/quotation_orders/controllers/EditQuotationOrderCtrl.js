05f5d18a46ac344cc6f2f8b02c888af0cc198e42
Egor Lobanov <egorlobanov@MacBook-Pro-Egor.local>
2016-03-03 15:32:01 +0300

    Ability to change product into "complete set"

--- app/quotation_orders/controllers/EditQuotationOrderCtrl.js
+++ app/quotation_orders/controllers/EditQuotationOrderCtrl.js
@@ -2,9 +2,10 @@
  * Created by Egor Lobanov on 23.11.15.
  */
 (function(){
-    angular.module('app.quotation_orders').run(function(editableOptions) {
+    angular.module('app.quotation_orders').run(['editableOptions', function(editableOptions) {
         editableOptions.theme = 'bs3';
-    }).controller('EditQuotationOrderCtrl', ['$scope', '$state', '$stateParams', 'serverApi', '$filter', 'funcFactory', function($scope, $state, $stateParams, serverApi, $filter, funcFactory){
+
+    }]).controller('EditQuotationOrderCtrl', ['$scope', '$state', '$stateParams', 'serverApi', '$filter', 'funcFactory', '$uibModal', function($scope, $state, $stateParams, serverApi, $filter, funcFactory, $uibModal){
         var sc = $scope;
         sc.data ={
             quotationOrder:{},
@@ -98,37 +99,71 @@
             // send changes on server
         };
 
-        sc.highlightElement = function(item){
-            for(var i=0; i < sc.data.quotationOrder.elements.length; i++){
-                if(sc.data.quotationOrder.elements[i].id===item.element_id){
-                    sc.data.quotationOrder.elements[i].highlight = true;
+        sc.changeProductModal = function(p){
+            var modalInstance = $uibModal.open({
+                templateUrl: 'eqoModal.tmpl.html',
+                controller: 'EqoModalInstanceCtrl',
+                windowClass: 'eqo-centred-modal',
+                resolve: {
+                    product : p.product
                 }
-            }
-        };
-        
-        sc.unhighlightElement = function(){
-            for(var i=0; i < sc.data.quotationOrder.elements.length; i++){
-                if(sc.data.quotationOrder.elements[i].highlight){
-                    sc.data.quotationOrder.elements[i].highlight = false;
-                }
-            }
-        };
-        
-        sc.highlightProduct = function(item){
-            for(var i=0; i < sc.data.quotationOrder.products.length; i++){
-                if(sc.data.quotationOrder.products[i].element_id===item.id){
-                    sc.data.quotationOrder.products[i].highlight = true;
-                }
-            }
-        };
-        
-        sc.unhighlightProduct = function(){
-            for(var i=0; i < sc.data.quotationOrder.products.length; i++){
-                if(sc.data.quotationOrder.products[i].highlight){
-                    sc.data.quotationOrder.products[i].highlight = false;
-                }
-            }
+            });
+
+            modalInstance.result.then(function (selectedProduct) {
+                p.product = selectedProduct;
+            });
         };
 
+//        sc.highlightElement = function(item){
+//            for(var i=0; i < sc.data.quotationOrder.elements.length; i++){
+//                if(sc.data.quotationOrder.elements[i].id===item.element_id){
+//                    sc.data.quotationOrder.elements[i].highlight = true;
+//                }
+//            }
+//        };
+//
+//        sc.unhighlightElement = function(){
+//            for(var i=0; i < sc.data.quotationOrder.elements.length; i++){
+//                if(sc.data.quotationOrder.elements[i].highlight){
+//                    sc.data.quotationOrder.elements[i].highlight = false;
+//                }
+//            }
+//        };
+//
+//        sc.highlightProduct = function(item){
+//            for(var i=0; i < sc.data.quotationOrder.products.length; i++){
+//                if(sc.data.quotationOrder.products[i].element_id===item.id){
+//                    sc.data.quotationOrder.products[i].highlight = true;
+//                }
+//            }
+//        };
+//
+//        sc.unhighlightProduct = function(){
+//            for(var i=0; i < sc.data.quotationOrder.products.length; i++){
+//                if(sc.data.quotationOrder.products[i].highlight){
+//                    sc.data.quotationOrder.products[i].highlight = false;
+//                }
+//            }
+//        };
+
+    }]).controller("EqoModalInstanceCtrl", ['$scope', '$uibModalInstance', 'serverApi', 'product', function($scope, $uibModalInstance, serverApi, product){
+        var sc = $scope;
+
+        sc.pSelectConfig = {
+            startPage: 0,
+            dataMethod: serverApi.getSearch
+        };
+        sc.data = {
+            selectedProduct: product,
+            productsList: []
+        };
+
+        sc.ok = function () {
+            $uibModalInstance.close(sc.data.selectedProduct);
+        };
+
+        sc.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
     }]);
 }());
