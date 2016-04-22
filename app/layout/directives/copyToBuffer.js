a05102de862cea1b0d66e9b12b0575a8446697ff
Egor Lobanov <trickster13r@icloud.com>
2016-04-23 01:02:03 +0300

    shake copy btn and show tooltip when value copied successfully

--- app/layout/directives/copyToBuffer.js
+++ app/layout/directives/copyToBuffer.js
@@ -9,13 +9,18 @@
 
     "use strict";
 
-    angular.module('teBuffer', []).directive('teCopyToBuffer', [function(){
+    angular.module('teBuffer', []).directive('teCopyToBuffer', ['$timeout', '$compile', function($timeout, $compile){
         return {
             restrict: 'A',
             transclude: true,
             link: function(scope, element, attrs){
                 var input = element.find(".te-c-buffer-input"),
-                    btn = element.find('.te-c-to-buffer-ico');
+                    btn = element.find('.te-c-to-buffer-ico'),
+                    iScope = scope.$new(true);
+
+                iScope.showTeCopyToBufferTooltip = false;
+
+                $compile(btn)(iScope);
 
                 var copy = function(){
                     var value = "",
@@ -33,6 +38,12 @@
                     input.focus();
                     input.select();
                     document.execCommand('copy');
+                    btn.addClass('animate');
+                    iScope.showTeCopyToBufferTooltip = true;
+                    $timeout(function(){
+                        btn.removeClass('animate');
+                        iScope.showTeCopyToBufferTooltip = false;
+                    },820);
                 };
 
                 btn.click(copy);
@@ -42,7 +53,7 @@
                 });
             },
             template: '<ng-transclude></ng-transclude>' +
-                '<i class="fa fa-copy te-c-to-buffer-ico">' +
+                '<i class="fa fa-copy te-c-to-buffer-ico" uib-tooltip="Скопировано!" tooltip-trigger="none" tooltip-is-open="showTeCopyToBufferTooltip">' +
                 '<input class="te-c-buffer-input" type="text">' +
                 '</i>'
         }
