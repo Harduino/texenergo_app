e8a50708514fe1c97175a26ff0c25dbf5feb7a42
Egor Lobanov <trickster13r@icloud.com>
2016-05-21 03:45:58 +0300

    ability to add and remove product into "receive orders"

--- app/receive_orders/controllers/ViewReceiveOrderCtrl.js
+++ app/receive_orders/controllers/ViewReceiveOrderCtrl.js
@@ -5,11 +5,12 @@
 
     "use strict";
 
-    angular.module('app.receive_orders').controller('ViewReceiveOrderCtrl', ['$scope', '$state', '$stateParams', 'serverApi', 'funcFactory', 'FileUploader', function($scope, $state, $stateParams, serverApi, funcFactory, FileUploader){
+    angular.module('app.receive_orders').controller('ViewReceiveOrderCtrl', ['$scope', '$state', '$stateParams', 'serverApi', 'funcFactory', 'FileUploader', 'CanCan', function($scope, $state, $stateParams, serverApi, funcFactory, FileUploader, CanCan){
         var sc = $scope;
         sc.receiveOrder = {};
         sc.visual = {
             navButtsOptions:[{type:'back', callback:returnBack}, {type:'edit', callback: goEditReceiveOrder}, {type:'refresh', callback:getReceiveOrderDetails}],
+            navTableButts:[{type:'remove', callback:deleteContentsProduct}],
             chartOptions: {
                 barColor:'rgb(103,135,155)',
                 scaleColor:false,
@@ -18,11 +19,24 @@
                 size:50
             },
             showFileModal: angular.noop,
-            titles: window.gon.index.ReceiveOrder.objectTitle + ': #'
+            titles: window.gon.index.ReceiveOrder.objectTitle + ': #',
+            roles: {
+                can_destroy: CanCan.can('edit', 'ReceiveOrder')
+            }
         };
 
         sc.amontPercent = 0;
         sc.receivedPercent = 0;
+
+        sc.productSelectConf = {
+            dataMethod: function(page, query, config, success){
+                if(page>1) success({data:[]});
+                else serverApi.getUnreceivedProducts($stateParams.id, query, config, success);
+            }
+        };
+
+        sc.ordersList = [];
+        sc.data = {};
         
         sc.uploader = new FileUploader({
             withCredentials: true,
@@ -37,7 +51,48 @@
                 }
             }
         });
-        
+
+        sc.onSelectProduct = function(item){
+            item.total_max = item.total;
+            item.quantity_max = item.quantity;
+        };
+
+        sc.appendOrder = function(event){
+            var append = function(){
+                var data = sc.data.productForAppend,
+                    post = {
+                        product_id: data.product.id,
+                        quantity: data.quantity,
+                        country: data.country || '',
+                        gtd: data.gtd || '',
+                        total_w_vat: data.total,
+                        supplier_order_id: data.supplier_order.id
+                    };
+                serverApi.createReceiveOrderContents(sc.receiveOrder.id, post, function(result){
+                    if(!result.data.errors){
+                        funcFactory.showNotification('Успешно', 'Продукт ' + data.product.name + ' успешно добвален', true);
+                        sc.receiveOrder.product_order_contents.push(angular.extend(data, result.data));
+                        clearProductForAppend();
+                        angular.element('#ero_prod_select').data().$uiSelectController.open=true;
+                    }else funcFactory.showNotification('Не удалось добавить продукт ' + data.product.name, result.data.errors);
+                });
+            };
+            var p = sc.data.productForAppend.product;
+            if(p && p.id) if(event){
+                event.keyCode == 13 && append();
+            }else append();
+        };
+
+        function deleteContentsProduct(item){
+            var data = item.data;
+            serverApi.deleteReceiveOrderContents(sc.receiveOrder.id, data.id, function(result){
+                if(!result.data.errors){
+                    funcFactory.showNotification('Успешно', 'Продукт ' + data.product.name + ' успешно удален', true);
+                    sc.receiveOrder.product_order_contents.splice(item.index, 1);
+                }else funcFactory.showNotification('Не удалось удалить продукт ' + data.product.name, result.data.errors);
+            });
+        }
+
         function setFileUploadOptions(receive_order){
             sc.uploader.url = 'http://www.texenergo.com/api/receive_orders/'+ receive_order.id +'/documents';
         }
@@ -53,6 +108,11 @@
 
             });
         }
+
+        function clearProductForAppend (){
+            sc.data.productForAppend={country:'',gtd:'', quantity:0, total:0}
+        }
+        clearProductForAppend();
 
         function returnBack(){
             $state.go('app.receive_orders',{});
