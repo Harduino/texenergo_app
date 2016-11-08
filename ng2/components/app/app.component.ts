1ec6834a43103a77be7fab2d9bd3a1a96357fb4f
Alex Elgin <sascha.elgin@gmail.com>
2016-11-08 11:10:29 +0600

    add a dummy ng2 app

---
+++ ng2/components/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng2-app',
+  template: ''
+})
+
+export class AppComponent { }
---
+++ ng2/modules/upgrade-adapter.module.ts
@@ -0,0 +1,5 @@
+import { UpgradeAdapter } from '@angular/upgrade';
+
+import { AppModule } from './app.module';
+
+export const upgradeAdapter = new UpgradeAdapter(AppModule);
