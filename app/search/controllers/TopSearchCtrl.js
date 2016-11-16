0ddcf2a65b1bdf74810dfa8b4ac0a0b3d83dc49e
Egor Lobanov <trickster13r@icloud.com>
2016-11-16 15:57:46 +0300

    Navigation inside top search results via arrows.

--- app/search/controllers/TopSearchCtrl.js
+++ app/search/controllers/TopSearchCtrl.js
@@ -6,8 +6,7 @@
 
         'use strict';
 
-        angular.module('app.search').controller('TopSearchCtrl', ['$scope', '$stateParams', '$location', '$state', 'serverApi', 'CanCan', '$uibModal', '$filter', function($scope, $stateParams, $location, $state, serverApi, CanCan, $uibModal, $filter){
-            var sc = $scope;
+        angular.module('app.search').controller('TopSearchCtrl', ['$scope', '$stateParams', '$location', '$state', 'serverApi', 'CanCan', '$uibModal', '$filter', function(sc, $stateParams, $location, $state, serverApi, CanCan, $uibModal, $filter){
 
             sc.data = {
                 searchText: decodeURIComponent($stateParams.searchString), //содержимое сроки поиска в топ меню
@@ -24,6 +23,11 @@
                     can_destroy: CanCan.can('Product', 'destroy')
                 }
             };
+
+            sc.selectedRowIndex = -1;
+
+            //add keydown listener to handle navigation inside results table
+            angular.element(window).on('keydown', navigateInTable);
 
             sc.searchProduct = function(pageNumber, query, options, callback){
                 serverApi.getSearch(pageNumber, query, options, callback);
@@ -85,7 +89,11 @@
                     });
                 }
             };
-            
+
+            sc.selectRow = function(index){
+                sc.selectedRowIndex = index;
+            };
+
             /**
              * Открываем модальное окно, чтобы открыть список заказов и добавить товар
              * @param p - product
@@ -108,13 +116,58 @@
             function viewProduct(id){
                 $state.go('app.product', {id: id});
             }
-            
+
             /**
-             * Открывает edit для редактирования товара
-             * @param id - id продукта информацию по которому необходимо посмотреть
+             * Navigate inside the table via arrows.
+             * Handler of keydown event.
+             * If Enter pressed, then open product view
+             * @param event
              */
-            function editProduct(id){
-                $state.go('app.product.edit', {id: id});
+            function navigateInTable (event){
+
+                const up = "ArrowUp";
+                const enter = 13;
+                const matcher = /ArrowUp|ArrowDown/;
+                var key = event.key,
+                    list = sc.data.searchList,
+                    _window = angular.element(window),
+                    scrollTop = _window.scrollTop(),
+                    outerHeight = _window.outerHeight() + scrollTop;
+
+
+                if(key.match(matcher)!== null && list.length>0){
+                    //select row first time
+                    if(sc.selectedRowIndex<0){
+                        sc.selectedRowIndex = 0;
+                    }else{
+                        var step = key === up ? -1 : 1,
+                            newIndex = sc.selectedRowIndex + step;
+
+                        if(newIndex >-1 && newIndex<sc.data.searchList.length){
+                            sc.selectedRowIndex = newIndex;
+
+                            const selectedIndexPosition = angular.element('#top_search_table_body>tr').eq(sc.selectedRowIndex).offset();
+
+                            if(selectedIndexPosition.top > outerHeight){
+                                _window.scrollTop(_window.scrollTop() + 200, 1000);
+                            }
+
+                            if(selectedIndexPosition.top < scrollTop){
+                                _window.scrollTop(_window.scrollTop() - 200, 1000);
+                            }
+                        }
+                    }
+                    //on hit Enter
+                }else if(event.keyCode === enter && list.length>0 && sc.selectedRowIndex > -1){
+
+                    var selectedRow = list[sc.selectedRowIndex];
+                    viewProduct(selectedRow._id || selectedRow.id);
+                }
             }
+
+            //clear resources before scope will be destroyed
+            sc.$on('$destroy', function(){
+                angular.element(window).off('keydown', navigateInTable);
+            });
         }]);
 }());
