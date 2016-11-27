ede5f5d3d831a8a62058351b8f3376491f765df4
Alex Elgin <sascha.elgin@gmail.com>
2016-11-27 11:51:51 +0600

    use `controllerAs` syntax in the 'Contacts' controller

--- app/contacts/module.js
+++ app/contacts/module.js
@@ -1,44 +1,34 @@
-/**
- * Created by Egor Lobanov on 07.11.15.
- * Модуль страницы контакты
- */
-(function () {
-
-    "use strict";
-
-    var module = angular.module('app.contacts', ['ui.router']);
-
-    module.config(function ($stateProvider) {
-        $stateProvider.state('app.contacts', {
-            url: '/contacts?q',
-            data:{
-                title: 'Контакты',
-                access:{
-                    action:'index',
-                    params:'Contact'
-                }
-            },
-            views:{
-                "content@app": {
-                    controller: 'ContactsCtrl',
-                    templateUrl: '/app/contacts/views/contacts.html'
-                }
+angular.module('app.contacts', ['ui.router']).config($stateProvider => {
+    $stateProvider.state('app.contacts', {
+        url: '/contacts?q',
+        data:{
+            title: 'Контакты',
+            access:{
+                action:'index',
+                params:'Contact'
             }
-        }).state('app.contacts.view', {
-            url: '/:id',
-            data:{
-                title: 'Просмотр контакта',
-                access:{
-                    action:'read',
-                    params:'Contact'
-                }
-            },
-            views:{
-                "content@app":{
-                    controller: 'ViewContactCtrl',
-                    templateUrl: '/app/contacts/views/viewContact.html'
-                }
+        },
+        views:{
+            "content@app": {
+                controller: 'ContactsCtrl',
+                controllerAs: 'contactsCtrl',
+                templateUrl: '/app/contacts/views/contacts.html'
             }
-        });
+        }
+    }).state('app.contacts.view', {
+        url: '/:id',
+        data:{
+            title: 'Просмотр контакта',
+            access:{
+                action:'read',
+                params:'Contact'
+            }
+        },
+        views:{
+            "content@app":{
+                controller: 'ViewContactCtrl',
+                templateUrl: '/app/contacts/views/viewContact.html'
+            }
+        }
     });
-}());
+});
