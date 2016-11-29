7250743f3e740df9c0e898f35c9215707aca618c
Alex Elgin <sascha.elgin@gmail.com>
2016-11-29 10:15:31 +0600

    use ES6 in the 'Dashboard' controller

--- app/dashboard/controllers/DashboardCtrl.js
+++ app/dashboard/controllers/DashboardCtrl.js
@@ -1,26 +1,21 @@
-(function () {
-    'use strict';
+class DashboardCtrl {
+    constructor(serverApi) {
+        let self = this;
+        this.data = {customerOrders:[], dispatchOrders:[], parseLogSearchQ: '', searchLogSearchQ: ''};
 
-    angular.module('app.dashboard').controller('DashboardCtrl', ['serverApi', function (serverApi) {
-        var self = this;
-
-        this.data = {customerOrders:[], dispatchOrders:[], parseLogSearchQ: "", searchLogSearchQ: ""};
         this.searchLogScrlCnfg = {dataMethod: serverApi.getSearchLogs, resultCollection: [], setQueryValue: angular.noop};
         this.parseLogScrlCnfg = {dataMethod: serverApi.getParseLogs, resultCollection: [], setQueryValue: angular.noop};
 
+        serverApi.getCustomerOrders(1, '', {}, result => self.data.customerOrders = result.data.slice(0, 10));
+        serverApi.getDispatchOrders(1, '', {}, result => self.data.dispatchOrders = result.data.slice(0, 10));
+    }
 
-        serverApi.getCustomerOrders(1, "", {}, function(result){
-            self.data.customerOrders = result.data.slice(0,10);
-        });
+    filterLogTable(e, type) {
+        if((e.keyCode == 13) || (e.type == 'click')) {
+            this[type + 'LogScrlCnfg'].setQueryValue(this.data[type + 'LogSearchQ']);
+        }
+    }
+}
 
-        serverApi.getDispatchOrders(1, "", {}, function(result){
-            self.data.dispatchOrders = result.data.slice(0,10);
-        });
-
-        this.filterLogTable = function(e, type) {
-            if(e.keyCode == 13 || e.type == "click"){
-                this[type + "LogScrlCnfg"].setQueryValue(self.data[type + "LogSearchQ"]);
-            }
-        };
-    }]);
-}());
+DashboardCtrl.$inject = ['serverApi'];
+angular.module('app.dashboard').controller('DashboardCtrl', DashboardCtrl);
