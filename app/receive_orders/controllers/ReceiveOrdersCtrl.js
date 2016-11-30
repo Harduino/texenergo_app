d614b8937eb3fb91b8797a1ed3b267d618166b51
Alex Elgin <sascha.elgin@gmail.com>
2016-11-30 23:28:38 +0600

    use ES6 in the 'Receive Orders' controller

--- app/receive_orders/controllers/ReceiveOrdersCtrl.js
+++ app/receive_orders/controllers/ReceiveOrdersCtrl.js
@@ -1,49 +1,48 @@
-/**
- * Created by Mikhail Arzhaev on 26.11.15.
- */
-(function(){
-    'use strict';
+class ReceiveOrdersCtrl {
+    constructor($state, $stateParams, serverApi, CanCan, funcFactory) {
+        let self = this;
 
-    angular.module('app.receive_orders').controller('ReceiveOrdersCtrl', ['$state', '$stateParams', 'serverApi', 'CanCan', 'funcFactory', function($state, $stateParams, serverApi, CanCan, funcFactory){
-        var self = this;
+        this.$state = $state;
+        this.serverApi = serverApi;
+        this.funcFactory = funcFactory;
 
         this.visual = {
             navButtsOptions:[
                 {
                     type: 'new',
-                    callback: function() {
+                    callback: () => {
                         self.newOrderData.date = new Date();
                         $('#createNewReceiveOrderModal').modal('show');
                     }
                 },
                 {
                     type: 'refresh',
-                    callback: function() {
-                        $state.go('app.receive_orders', {}, {reload:true});
-                    }
+                    callback: () => $state.go('app.receive_orders', {}, {reload:true})
                 }
             ],
             navTableButts:[
                 {
                     type: 'view',
-                    callback: function(item) {
-                        $state.go('app.receive_orders.view', {id:item.data.id || item.data._id});
-                    }
+                    callback: item => $state.go('app.receive_orders.view', {id: item.data.id || item.data._id})
                 },
                 {
                     type: 'remove',
-                    callback: function(item) {
+                    callback: item => {
                         $.SmartMessageBox({
-                            title: "Удалить заказ?",
-                            content: "Вы действительно хотите удалить поступление " + item.data.number,
+                            title: 'Удалить заказ?',
+                            content: 'Вы действительно хотите удалить поступление ' + item.data.number,
                             buttons: '[Нет][Да]'
-                        }, function (ButtonPressed) {
-                            if (ButtonPressed === "Да") {
-                                serverApi.deleteReceiveOrder(item.data.id, function(result){
+                        }, ButtonPressed => {
+                            if (ButtonPressed === 'Да') {
+                                serverApi.deleteReceiveOrder(item.data.id, result => {
                                     if(!result.data.errors){
                                         self.data.ordersList.splice(item.index,1);
-                                        funcFactory.showNotification('Поступление ' + item.data.number + ' успешно удалено.', '', true);
-                                    }else funcFactory.showNotification('Не удалось удалить поступление ' + item.data.number, result.data.errors);
+                                        funcFactory.showNotification('Поступление ' + item.data.number +
+                                            ' успешно удалено.', '', true);
+                                    } else {
+                                        funcFactory.showNotification('Не удалось удалить поступление ' +
+                                            item.data.number, result.data.errors);
+                                    }
                                 });
                             }
                         });
@@ -54,35 +53,40 @@
                 can_edit: CanCan.can('edit', 'ReceiveOrder'),
                 can_destroy: CanCan.can('destroy', 'ReceiveOrder')
             },
-            titles: ["Поступления"]
+            titles: ['Поступления']
         };
 
         this.data = {ordersList:[], partnersList:[], searchQuery: $stateParams.q};
         this.partnerSelectConfig = {dataMethod: serverApi.getPartners};
         this.newOrderData = {};
-        
-        this.addNewOrder = function(){
-            var data = {
-                date: self.newOrderData.date,
-                number: self.newOrderData.number,
-                vat_code: self.newOrderData.vat_code,
-                partner_id: self.newOrderData.partner.id
-            };
+    }
 
-            serverApi.createReceiveOrder(data, function(result) {
-                if(!result.data.errors) {
-                    self.data.ordersList.unshift(result.data);
-                    funcFactory.showNotification('Поступление успешно создано', '', true);
-                    self.clearCreateOrder();
-                    $state.go('app.receive_orders.view', {id: result.data.id});
-                } else {
-                    funcFactory.showNotification('Не удалось создать поступление', result.data.errors);
-                }
-            });
+    addNewOrder() {
+        let data = {
+            date: this.newOrderData.date,
+            number: this.newOrderData.number,
+            vat_code: this.newOrderData.vat_code,
+            partner_id: this.newOrderData.partner.id
         };
 
-        this.clearCreateOrder = function(){
-            self.newOrderData = {};
-        };
-    }]);
-}());
+        let self = this;
+
+        this.serverApi.createReceiveOrder(data, result => {
+            if(!result.data.errors) {
+                self.data.ordersList.unshift(result.data);
+                self.funcFactory.showNotification('Поступление успешно создано', '', true);
+                self.clearCreateOrder();
+                self.$state.go('app.receive_orders.view', {id: result.data.id});
+            } else {
+                self.funcFactory.showNotification('Не удалось создать поступление', result.data.errors);
+            }
+        });
+    }
+
+    clearCreateOrder() {
+        this.newOrderData = {};
+    }
+}
+
+ReceiveOrdersCtrl.$inject = ['$state', '$stateParams', 'serverApi', 'CanCan', 'funcFactory'];
+angular.module('app.receive_orders').controller('ReceiveOrdersCtrl', ReceiveOrdersCtrl);
