9eb58ab7bfebd7a9bcc4eb3e371eef2adedabc5a
Alex Elgin <sascha.elgin@gmail.com>
2016-12-09 16:51:40 +0600

    use `controllerAs` syntax in the 'Partners' controller

--- app/partners/controllers/PartnersCtrl.js
+++ app/partners/controllers/PartnersCtrl.js
@@ -2,58 +2,52 @@
  * Created by Mikhail Arzhaev on 26.11.15.
  */
 (function(){
-
     'use strict';
 
-    angular.module('app.partners').controller('PartnersCtrl', ['$scope', '$state', '$stateParams', 'serverApi', '$filter', 'funcFactory', '$parse', function($scope, $state, $stateParams, serverApi, $filter, funcFactory, $parse){
-        var sc = $scope;
+    angular.module('app.partners').controller('PartnersCtrl', ['$state', '$stateParams', 'serverApi', '$filter', 'funcFactory', '$parse', function($state, $stateParams, serverApi, $filter, funcFactory, $parse){
+        var self = this;
 
-        sc.visual = {
+        this.visual = {
             navButtsOptions: [
-                { type: 'new', callback: createPartner }
+                {
+                    type: 'new',
+                    callback: function() {
+                        $('#createPartnerModal').modal('show');
+                    }
+                }
             ],
             navTableButts: [
-                { type: 'view', callback: viewPartner },
+                {
+                    type: 'view',
+                    callback: function(data) {
+                        $state.go('app.partners.view', {id: data.id || data._id});
+                    }
+                },
                 { type: 'remove' }
             ],
-            titles: ["Партнёры"]
+            titles: ['Партнёры']
         };
         
-        sc.data = {
-            partnersList:[],
-            searchQuery:$stateParams.q
-        };
+        this.data = {partnersList: [], searchQuery: $stateParams.q};
+        this.newPartnerData = {inn: '', name: '', delivery_address: '', phone: '', email: ''};
         
-        sc.newPartnerData = {
-            inn: '',
-            name: '',
-            delivery_address: '',
-            phone: '',
-            email: ''
-        };
-        
-        sc.addNewPartner = function(){
-            serverApi.createPartner(sc.newPartnerData, function(result){
+        this.addNewPartner = function(){
+            serverApi.createPartner(self.newPartnerData, function(result){
                 if(!result.data.errors){
-                    sc.data.partnersList.unshift(result.data);
+                    self.data.partnersList.unshift(result.data);
                     funcFactory.showNotification('Партнёр успешно добавлен', '', true);
-                    sc.clearNewPartnerData();
+                    self.clearNewPartnerData();
                 } else {
                     funcFactory.showNotification('Не удалось создать партнёра', result.data.errors);
-                };
+                }
             });
         };
 
-        sc.clearNewPartnerData = function(){
-            sc.newPartnerData = {
-                inn: '',
-                name: '',
-                delivery_address: '',
-                phone: ''
-            };
+        this.clearNewPartnerData = function(){
+            self.newPartnerData = {inn: '', name: '', delivery_address: '', phone: ''};
         };
 
-        sc.getDaDataSuggestions = function(type,val, field_name){
+        this.getDaDataSuggestions = function(type,val, field_name){
             return serverApi.validateViaDaData(type, {"query": val}).then(function(result){
                 return result.data.suggestions.map(function(item){
                     return {label: $parse(field_name)(item) || val, item: item, field: field_name};
@@ -61,9 +55,9 @@
             });
         };
 
-        sc.fillBySuggestion = function($item, prop){
+        this.fillBySuggestion = function($item, prop){
             var data = $item.item.data,
-                addr = sc.newPartnerData[prop];
+                addr = self.newPartnerData[prop];
 
             switch ($item.field){
                 case 'data.postal_code': {
@@ -91,14 +85,16 @@
                     break;
                 }
                 case 'data.inn' : {
-                    sc.newPartnerData.inn = data.inn;
-                    sc.newPartnerData.kpp = (data.kpp || "0");
-                    sc.newPartnerData.legal_name = data.name.short_with_opf;
-                    sc.newPartnerData.name = (data.name.short || data.name.short);
+                    self.newPartnerData.inn = data.inn;
+                    self.newPartnerData.kpp = (data.kpp || "0");
+                    self.newPartnerData.legal_name = data.name.short_with_opf;
+                    self.newPartnerData.name = (data.name.short || data.name.short);
+
                     if(data.management !== undefined) {
-                        sc.newPartnerData.ceo_name = data.management.name;
-                        sc.newPartnerData.ceo_title = data.management.post;
+                        self.newPartnerData.ceo_name = data.management.name;
+                        self.newPartnerData.ceo_title = data.management.post;
                     }
+
                     if(data.address.data !== null && data.address.data !== undefined) {
                         var address = {
                             postal_index: data.address.data.postal_code,
@@ -106,9 +102,10 @@
                             city: (data.address.data.city || data.address.data.settlement_with_type),
                             street: data.address.data.street_with_type,
                             house: data.address.data.house
-                        }
-                        sc.newPartnerData.legal_address = address;
-                        sc.newPartnerData.delivery_address = address;
+                        };
+
+                        self.newPartnerData.legal_address = address;
+                        self.newPartnerData.delivery_address = address;
                     }
                     break;
                 }
@@ -116,16 +113,8 @@
             }
         };
         
-        sc.onAddFile = function(){
+        this.onAddFile = function(){
             //this.parentNode.nextSibling.value = this.value;
         };
-
-        function viewPartner(data){
-            $state.go('app.partners.view', {id:data.id || data._id});
-        }
-
-        function createPartner(){
-            $('#createPartnerModal').modal('show');
-        }
     }]);
 }());
--- app/partners/module.js
+++ app/partners/module.js
@@ -1,67 +1,62 @@
-/**
- * Created by Egor Lobanov on 17.11.15.
- * Модуль страницы "Партнер"
- */
-(function (){
-
-    "use strict";
-
-    var module = angular.module('app.partners', ['ui.router']);
-
-    module.config(function ($stateProvider) {
-        $stateProvider.state('app.partners', {
-            url: '/partners?q',
-            data:{
-                title: 'Партнёры',
-                access:{
-                    action:'index',
-                    params:'Partner'
+angular.module('app.partners', ['ui.router'])
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('app.partners', {
+                url: '/partners?q',
+                data: {
+                    title: 'Партнёры',
+                    access: {
+                        action: 'index',
+                        params: 'Partner'
+                    }
+                },
+                params: {
+                  q: ''
+                },
+                views:{
+                    "content@app": {
+                        controller: 'PartnersCtrl',
+                        controllerAs: 'partnersCtrl',
+                        templateUrl: '/app/partners/views/partners.html'
+                    }
                 }
-            },
-            params:{
-              q:''
-            },
-            views:{
-                "content@app": {
-                    controller: 'PartnersCtrl',
-                    templateUrl: '/app/partners/views/partners.html'
+            })
+            .state('app.partners.view', {
+                url: '/:id',
+                data: {
+                    access: {
+                        action: 'show',
+                        params: 'Partner'
+                    }
+                },
+                views:{
+                    "content@app": {
+                        controller: 'ViewPartnerCtrl',
+                        templateUrl: '/app/partners/views/showPartner.html'
+                    }
                 }
-            }
-        }).state('app.partners.view', {
-            url: '/:id',
-            data:{
-                access:{
-                    action:'show',
-                    params:'Partner'
+            })
+            .state('app.partners.view.logs', {
+                url: '/logs',
+                data: {
+                    title: 'История партнёра',
+                    access: {
+                        action: 'logs',
+                        params: 'Partner'
+                    }
+                },
+                views:{
+                    "content@app":{
+                        controller: 'LogsPartnerCtrl',
+                        templateUrl: '/app/partners/views/logsPartner.html'
+                    }
                 }
-            },
-            views:{
-                "content@app":{
-                    controller: 'ViewPartnerCtrl',
-                    templateUrl: '/app/partners/views/showPartner.html'
-                }
-            }
-        }).state('app.partners.view.logs', {
-            url: '/logs',
-            data:{
-                title:'История партнёра',
-                access:{
-                    action:'logs',
-                    params:'Partner'
-                }
-            },
-            views:{
-                "content@app":{
-                    controller: 'LogsPartnerCtrl',
-                    templateUrl: '/app/partners/views/logsPartner.html'
-                }
-            }
-        });
-    });
-
-    module.filter('innKpp', function(){
-       return function(inn, kpi){
-           return inn && kpi ? inn.toString() + "/" + kpi.toString() : '';
+            })
+        ;
+    })
+    .filter('innKpp', function() {
+       return function(inn, kpi) {
+           return inn && kpi ? inn.toString() + '/' + kpi.toString() : '';
        }
-    });
-}());
+    })
+;
