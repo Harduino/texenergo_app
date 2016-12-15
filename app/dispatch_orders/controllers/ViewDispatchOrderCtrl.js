a71f569db4ece76e17763fcd0ecf7dd6a2ec3e79
Alex Elgin <sascha.elgin@gmail.com>
2016-12-15 11:57:50 +0600

    use `controllerAs` syntax in the 'View Dispatch Order' controller

--- app/dispatch_orders/controllers/ViewDispatchOrderCtrl.js
+++ app/dispatch_orders/controllers/ViewDispatchOrderCtrl.js
@@ -1,126 +1,123 @@
-/**
- * Created by Mikhail Arzhaev 26.11.15
- */
-(function(){
+angular.module('app.dispatch_orders').controller('ViewDispatchOrderCtrl', ['$state', '$stateParams', 'serverApi', 'funcFactory', 'FileUploader', '$http', '$localStorage', function($state, $stateParams, serverApi, funcFactory, FileUploader, $http, $localStorage){
+    var self = this;
+    this.dispatchOrder = {};
 
-    "use strict";
-
-    angular.module('app.dispatch_orders').controller('ViewDispatchOrderCtrl', ['$scope', '$state', '$stateParams', 'serverApi', 'funcFactory', 'FileUploader', '$http', '$localStorage', function($scope, $state, $stateParams, serverApi, funcFactory, FileUploader, $http, $localStorage){
-        var _pattern = /\/?#/;
-
-        var sc = $scope;
-        sc.dispatchOrder = {};
-        sc.visual = {
-            navButtsOptions:[
-                { type: 'back', callback: returnBack },
-                { type: 'upd_form_pdf', callback: openDispatchOrderPdf },
-                { type: 'label_pdf', callback: openLabelPdf },
-                { type: 'packing_list_pdf', callback: openPackingListPdf },
-                { type: 'confirm_order', callback: updateStatusDispatchOrder },
-                { type: 'refresh', callback: getDispatchOrderDetails },
-                { type: 'logs', callback: goToLogs }
-            ],
-            chartOptions: {
-                barColor:'rgb(103,135,155)',
-                scaleColor:false,
-                lineWidth:5,
-                lineCap:'circle',
-                size:50
+    this.visual = {
+        navButtsOptions:[
+            {
+                type: 'back',
+                callback: function() {
+                    $state.go('app.dispatch_orders',{});
+                }
             },
-            showFileModal: angular.noop,
-            titles: 'Списание: №'
-        };
-
-        sc.amontPercent = 0;
-        sc.dispatchedPercent = 0;
-
-        sc.uploader = new FileUploader({
-            withCredentials: true,
-            queueLimit: 5,
-            onCompleteItem: function(fileItem, response, status, headers) {
-                if(status===200){
-                    sc.receiveOrder.documents.push(response);
-                    sc.uploader.clearQueue();
-                    funcFactory.showNotification("Успешно", 'Приложил документ.',true);
-                } else {
-                    funcFactory.showNotification('Не удалось приложить документ', response.errors);   
+            {
+                type: 'upd_form_pdf',
+                callback: function() {
+                    openPdf('');
+                }
+            },
+            {
+                type: 'label_pdf',
+                callback: function() {
+                    openPdf('/label');
+                }
+            },
+            {
+                type: 'packing_list_pdf',
+                callback: function() {
+                    openPdf('/packing_list');
+                }
+            },
+            {
+                type: 'confirm_order',
+                callback: function(subdata, item) {
+                    var data = {
+                        dispatch_order:{
+                            event: item.event
+                        }
+                    };
+                    serverApi.updateStatusDispatchOrder($stateParams.id, data, function(result){
+                        if(result.status == 200 && !result.data.errors) {
+                            funcFactory.showNotification("Успешно", 'Удалось ' + item.name.toLowerCase() + ' заказ', true);
+                            self.dispatchOrder = result.data;
+                        } else {
+                            funcFactory.showNotification("Не удалось " + item.name.toLowerCase() + ' заказ', result.data.errors);
+                        }
+                    });
+                }
+            },
+            {
+                type: 'refresh',
+                callback: getDispatchOrderDetails
+            },
+            {
+                type: 'logs',
+                callback: function() {
+                    $state.go('app.dispatch_orders.view.logs', {});
                 }
             }
+        ],
+        chartOptions: {
+            barColor:'rgb(103,135,155)',
+            scaleColor:false,
+            lineWidth:5,
+            lineCap:'circle',
+            size:50
+        },
+        showFileModal: angular.noop,
+        titles: 'Списание: №'
+    };
+
+    this.amontPercent = 0;
+    this.dispatchedPercent = 0;
+
+    this.uploader = new FileUploader({
+        withCredentials: true,
+        queueLimit: 5,
+        onCompleteItem: function(fileItem, response, status) {
+            if(status === 200){
+                self.dispatchOrder.documents.push(response);
+                self.uploader.clearQueue();
+                funcFactory.showNotification("Успешно", 'Приложил документ.', true);
+            } else {
+                funcFactory.showNotification('Не удалось приложить документ', response.errors);
+            }
+        }
+    });
+
+    function setFileUploadOptions(dispatch_order){
+        self.uploader.url = window.APP_ENV.TEXENERGO_COM_API_HTTP_BASE_URL + '/dispatch_order/' + dispatch_order.id +
+            '/documents';
+    }
+
+    getDispatchOrderDetails();
+
+    function getDispatchOrderDetails(){
+        serverApi.getDispatchOrderDetails($stateParams.id, function(result){
+
+            var dispatchOrder = self.dispatchOrder = result.data;
+            self.amontPercent = funcFactory.getPercent(dispatchOrder.paid_amount, dispatchOrder.total);
+            self.dispatchedPercent = funcFactory.getPercent(dispatchOrder.dispatched_amount, dispatchOrder.total);
+
+            setFileUploadOptions(dispatchOrder);
         });
-        
-        function setFileUploadOptions(dispatch_order){
-            sc.uploader.url = window.APP_ENV.TEXENERGO_COM_API_HTTP_BASE_URL + '/dispatch_order/' + dispatch_order.id +
-                '/documents';
-        }
+    }
 
-        getDispatchOrderDetails();
+    function openPdf(path) {
+        window.open(window.APP_ENV.TEXENERGO_COM_API_HTTP_BASE_URL + '/dispatch_orders/' + self.dispatchOrder.id + path +
+            '.pdf?token=' + $localStorage.id_token, '_blank');
+    }
 
-        function getDispatchOrderDetails(){
-            serverApi.getDispatchOrderDetails($stateParams.id, function(result){
+    this.saveChosenPerson = function(){
+        var data = {dispatch_order: {person_id: self.dispatchOrder.chosenPersonId}};
 
-                var dispatchOrder = sc.dispatchOrder = result.data;
-                sc.amontPercent = funcFactory.getPercent(dispatchOrder.paid_amount, dispatchOrder.total);
-                sc.dispatchedPercent = funcFactory.getPercent(dispatchOrder.dispatched_amount, dispatchOrder.total);
-
-                setFileUploadOptions(dispatchOrder);
-            });
-        }
-
-        function returnBack(){
-            $state.go('app.dispatch_orders',{});
-        }
-
-        function openPdf(path) {
-            window.open(window.APP_ENV.TEXENERGO_COM_API_HTTP_BASE_URL + '/dispatch_orders/' + sc.dispatchOrder.id + path +
-                '.pdf?token=' + $localStorage.id_token, '_blank');
-        }
-        
-        function openDispatchOrderPdf(){
-            openPdf('');
-        }
-        
-        function openLabelPdf(){
-            openPdf('/label');
-        }
-        
-        function openPackingListPdf(){
-            openPdf('/packing_list');
-        }
-        
-        sc.saveChosenPerson = function(){
-            var data = {
-                dispatch_order:{
-                    person_id: sc.dispatchOrder.chosenPersonId
-                }
-            };
-            serverApi.updateDispatchOrder(sc.dispatchOrder.id, data, function(result){
-                if(!result.data.errors){
-                    sc.dispatchOrder = result.data;
-                    funcFactory.showNotification("Успешно", 'Человек выбран.',true);
-                } else {
-                    funcFactory.showNotification('Ошибка при выборе человека', result.data.errors);
-                }
-            });
-        };
-
-        function updateStatusDispatchOrder(subdata, item) {
-            var data = {
-                dispatch_order:{
-                    event: item.event
-                }
-            };
-            serverApi.updateStatusDispatchOrder($stateParams.id, data, function(result){
-                if(result.status == 200 && !result.data.errors) {
-                    funcFactory.showNotification("Успешно", 'Удалось ' + item.name.toLowerCase() + ' заказ', true);
-                    sc.dispatchOrder = result.data;
-                } else {
-                    funcFactory.showNotification("Не удалось " + item.name.toLowerCase() + ' заказ', result.data.errors);
-                }
-            });
-        }
-
-        function goToLogs(){
-            $state.go('app.dispatch_orders.view.logs', {});
-        }
-    }]);
-}());
+        serverApi.updateDispatchOrder(self.dispatchOrder.id, data, function(result){
+            if(!result.data.errors){
+                self.dispatchOrder = result.data;
+                funcFactory.showNotification("Успешно", 'Человек выбран.',true);
+            } else {
+                funcFactory.showNotification('Ошибка при выборе человека', result.data.errors);
+            }
+        });
+    };
+}]);
--- app/dispatch_orders/module.js
+++ app/dispatch_orders/module.js
@@ -1,50 +1,57 @@
-angular.module('app.dispatch_orders', ['ui.router']).config($stateProvider => {
-    $stateProvider.state('app.dispatch_orders', {
-        url: '/dispatch_orders?q',
-        data:{
-            title: 'Списания',
-            access:{
-                action:'index',
-                params:'DispatchOrder'
-            }
-        },
-        params:{
-            q:''
-        },
-        views:{
-            "content@app": {
-                template: '<dispatch-orders></dispatch-orders>'
-            }
-        }
-    }).state('app.dispatch_orders.view', {
-        url: '/:id',
-        data:{
-            title: 'Просмотр списания',
-            access:{
-                action:'read',
-                params:'DispatchOrder'
-            }
-        },
-        views:{
-            "content@app":{
-                controller: 'ViewDispatchOrderCtrl',
-                templateUrl: '/app/dispatch_orders/views/viewDispatchOrder.html'
-            }
-        }
-    }).state('app.dispatch_orders.view.logs', {
-        url: '/logs',
-        data:{
-            title:'История списания',
-            access:{
-                action:'logs',
-                params:'DispatchOrder'
-            }
-        },
-        views:{
-            "content@app":{
-                controller: 'LogsDispatchOrderCtrl',
-                templateUrl: '/app/dispatch_orders/views/logsDispatchOrder.html'
-            }
-        }
-    });
-});
+angular.module('app.dispatch_orders', ['ui.router'])
+    .config($stateProvider => {
+        $stateProvider
+            .state('app.dispatch_orders', {
+                url: '/dispatch_orders?q',
+                data:{
+                    title: 'Списания',
+                    access:{
+                        action:'index',
+                        params:'DispatchOrder'
+                    }
+                },
+                params:{
+                    q:''
+                },
+                views:{
+                    'content@app': {
+                        template: '<dispatch-orders></dispatch-orders>'
+                    }
+                }
+            })
+            .state('app.dispatch_orders.view', {
+                url: '/:id',
+                data:{
+                    title: 'Просмотр списания',
+                    access:{
+                        action:'read',
+                        params:'DispatchOrder'
+                    }
+                },
+                views:{
+                    'content@app':{
+                        controller: 'ViewDispatchOrderCtrl',
+                        controllerAs: 'viewDispatchOrderCtrl',
+                        templateUrl: '/app/dispatch_orders/views/viewDispatchOrder.html'
+                    }
+                }
+            })
+            .state('app.dispatch_orders.view.logs', {
+                url: '/logs',
+                data:{
+                    title:'История списания',
+                    access:{
+                        action:'logs',
+                        params:'DispatchOrder'
+                    }
+                },
+                views:{
+                    'content@app':{
+                        controller: 'LogsDispatchOrderCtrl',
+                        templateUrl: '/app/dispatch_orders/views/logsDispatchOrder.html'
+                    }
+                }
+            })
+        ;
+    })
+;
