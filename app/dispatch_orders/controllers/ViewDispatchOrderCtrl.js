5f233cd4c7ec141170c257eb36560091dbd9d976
Alex Elgin <sascha.elgin@gmail.com>
2016-12-15 12:58:03 +0600

    use ES6 in the 'View Dispatch Order' controller

--- app/dispatch_orders/controllers/ViewDispatchOrderCtrl.js
+++ app/dispatch_orders/controllers/ViewDispatchOrderCtrl.js
@@ -1,123 +1,102 @@
-angular.module('app.dispatch_orders').controller('ViewDispatchOrderCtrl', ['$state', '$stateParams', 'serverApi', 'funcFactory', 'FileUploader', '$http', '$localStorage', function($state, $stateParams, serverApi, funcFactory, FileUploader, $http, $localStorage){
-    var self = this;
-    this.dispatchOrder = {};
+class ViewDispatchOrderCtrl {
+    constructor($state, $stateParams, serverApi, funcFactory, FileUploader, $localStorage) {
+        this.$stateParams = $stateParams;
+        this.serverApi = serverApi;
+        this.funcFactory = funcFactory;
+        this.$localStorage = $localStorage;
 
-    this.visual = {
-        navButtsOptions:[
-            {
-                type: 'back',
-                callback: function() {
-                    $state.go('app.dispatch_orders',{});
-                }
+        let self = this;
+        this.dispatchOrder = {};
+
+        this.visual = {
+            navButtsOptions:[
+                {type: 'back', callback: () => $state.go('app.dispatch_orders',{})},
+                {type: 'upd_form_pdf', callback: () => self.openPdf('')},
+                {type: 'label_pdf', callback: () => self.openPdf('/label')},
+                {type: 'packing_list_pdf', callback: () => self.openPdf('/packing_list')},
+                {
+                    type: 'confirm_order',
+                    callback: (subdata, item) => {
+                        let data = {dispatch_order: {event: item.event}};
+
+                        serverApi.updateStatusDispatchOrder($stateParams.id, data, result => {
+                            if(result.status == 200 && !result.data.errors) {
+                                funcFactory.showNotification('Успешно', 'Удалось ' + item.name.toLowerCase() + ' заказ',
+                                    true);
+                                self.dispatchOrder = result.data;
+                            } else {
+                                funcFactory.showNotification('Не удалось ' + item.name.toLowerCase() + ' заказ',
+                                    result.data.errors);
+                            }
+                        });
+                    }
+                },
+                {type: 'refresh', callback: () => self.getDispatchOrderDetails()},
+                {type: 'logs', callback: () => $state.go('app.dispatch_orders.view.logs', {})}
+            ],
+            chartOptions: {
+                barColor: 'rgb(103,135,155)',
+                scaleColor: false,
+                lineWidth: 5,
+                lineCap: 'circle',
+                size: 50
             },
-            {
-                type: 'upd_form_pdf',
-                callback: function() {
-                    openPdf('');
-                }
-            },
-            {
-                type: 'label_pdf',
-                callback: function() {
-                    openPdf('/label');
-                }
-            },
-            {
-                type: 'packing_list_pdf',
-                callback: function() {
-                    openPdf('/packing_list');
-                }
-            },
-            {
-                type: 'confirm_order',
-                callback: function(subdata, item) {
-                    var data = {
-                        dispatch_order:{
-                            event: item.event
-                        }
-                    };
-                    serverApi.updateStatusDispatchOrder($stateParams.id, data, function(result){
-                        if(result.status == 200 && !result.data.errors) {
-                            funcFactory.showNotification("Успешно", 'Удалось ' + item.name.toLowerCase() + ' заказ', true);
-                            self.dispatchOrder = result.data;
-                        } else {
-                            funcFactory.showNotification("Не удалось " + item.name.toLowerCase() + ' заказ', result.data.errors);
-                        }
-                    });
-                }
-            },
-            {
-                type: 'refresh',
-                callback: getDispatchOrderDetails
-            },
-            {
-                type: 'logs',
-                callback: function() {
-                    $state.go('app.dispatch_orders.view.logs', {});
+            showFileModal: angular.noop,
+            titles: 'Списание: №'
+        };
+
+        this.uploader = new FileUploader({
+            withCredentials: true,
+            queueLimit: 5,
+            onCompleteItem: (fileItem, response, status) => {
+                if(status === 200) {
+                    self.dispatchOrder.documents.push(response);
+                    self.uploader.clearQueue();
+                    funcFactory.showNotification('Успешно', 'Приложил документ.', true);
+                } else {
+                    funcFactory.showNotification('Не удалось приложить документ', response.errors);
                 }
             }
-        ],
-        chartOptions: {
-            barColor:'rgb(103,135,155)',
-            scaleColor:false,
-            lineWidth:5,
-            lineCap:'circle',
-            size:50
-        },
-        showFileModal: angular.noop,
-        titles: 'Списание: №'
-    };
+        });
 
-    this.amontPercent = 0;
-    this.dispatchedPercent = 0;
+        this.getDispatchOrderDetails();
+    }
 
-    this.uploader = new FileUploader({
-        withCredentials: true,
-        queueLimit: 5,
-        onCompleteItem: function(fileItem, response, status) {
-            if(status === 200){
-                self.dispatchOrder.documents.push(response);
-                self.uploader.clearQueue();
-                funcFactory.showNotification("Успешно", 'Приложил документ.', true);
+    saveChosenPerson() {
+        let data = {dispatch_order: {person_id: this.dispatchOrder.chosenPersonId}};
+        let self = this;
+
+        this.serverApi.updateDispatchOrder(this.dispatchOrder.id, data, result => {
+            if(result.data.errors) {
+                self.funcFactory.showNotification('Ошибка при выборе человека', result.data.errors);
             } else {
-                funcFactory.showNotification('Не удалось приложить документ', response.errors);
+                self.dispatchOrder = result.data;
+                self.funcFactory.showNotification('Успешно', 'Человек выбран.', true);
             }
-        }
-    });
+        });
+    }
 
-    function setFileUploadOptions(dispatch_order){
-        self.uploader.url = window.APP_ENV.TEXENERGO_COM_API_HTTP_BASE_URL + '/dispatch_order/' + dispatch_order.id +
+    getDispatchOrderDetails() {
+        let self = this;
+
+        this.serverApi.getDispatchOrderDetails(this.$stateParams.id, result => {
+            let dispatchOrder = self.dispatchOrder = result.data;
+            self.amontPercent = self.funcFactory.getPercent(dispatchOrder.paid_amount, dispatchOrder.total);
+            self.dispatchedPercent = self.funcFactory.getPercent(dispatchOrder.dispatched_amount, dispatchOrder.total);
+            self.setFileUploadOptions(dispatchOrder);
+        });
+    }
+
+    setFileUploadOptions(dispatchOrder) {
+        this.uploader.url = window.APP_ENV.TEXENERGO_COM_API_HTTP_BASE_URL + '/dispatch_order/' + dispatchOrder.id +
             '/documents';
     }
 
-    getDispatchOrderDetails();
-
-    function getDispatchOrderDetails(){
-        serverApi.getDispatchOrderDetails($stateParams.id, function(result){
-
-            var dispatchOrder = self.dispatchOrder = result.data;
-            self.amontPercent = funcFactory.getPercent(dispatchOrder.paid_amount, dispatchOrder.total);
-            self.dispatchedPercent = funcFactory.getPercent(dispatchOrder.dispatched_amount, dispatchOrder.total);
-
-            setFileUploadOptions(dispatchOrder);
-        });
+    openPdf(path) {
+        window.open(window.APP_ENV.TEXENERGO_COM_API_HTTP_BASE_URL + '/dispatch_orders/' + this.dispatchOrder.id +
+            path + '.pdf?token=' + this.$localStorage.id_token, '_blank');
     }
+}
 
-    function openPdf(path) {
-        window.open(window.APP_ENV.TEXENERGO_COM_API_HTTP_BASE_URL + '/dispatch_orders/' + self.dispatchOrder.id + path +
-            '.pdf?token=' + $localStorage.id_token, '_blank');
-    }
-
-    this.saveChosenPerson = function(){
-        var data = {dispatch_order: {person_id: self.dispatchOrder.chosenPersonId}};
-
-        serverApi.updateDispatchOrder(self.dispatchOrder.id, data, function(result){
-            if(!result.data.errors){
-                self.dispatchOrder = result.data;
-                funcFactory.showNotification("Успешно", 'Человек выбран.',true);
-            } else {
-                funcFactory.showNotification('Ошибка при выборе человека', result.data.errors);
-            }
-        });
-    };
-}]);
+ViewDispatchOrderCtrl.$inject = ['$state', '$stateParams', 'serverApi', 'funcFactory', 'FileUploader', '$localStorage'];
+angular.module('app.dispatch_orders').controller('ViewDispatchOrderCtrl', ViewDispatchOrderCtrl);
