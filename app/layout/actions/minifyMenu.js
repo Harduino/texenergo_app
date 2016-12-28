69e52af98812cf837618e5d5fd3d77ae980ecda3
Alex Elgin <sascha.elgin@gmail.com>
2016-12-28 15:56:23 +0600

    use element selector in the `minify-menu` directive

--- app/layout/actions/minifyMenu.js
+++ app/layout/actions/minifyMenu.js
@@ -3,7 +3,7 @@
 
     angular.module('app.layout').directive('minifyMenu', function(){
         return {
-            restrict: 'A',
+            restrict: 'E',
             link: function(scope, element){
                     var $body = $('body');
                 var minifyMenu = function() {
