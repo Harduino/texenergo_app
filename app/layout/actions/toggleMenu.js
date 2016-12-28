12b72401c8cd8e3daeb0b6f210f96e1ad83b2558
Alex Elgin <sascha.elgin@gmail.com>
2016-12-28 17:20:03 +0600

    use element selector in the `toggle-menu` directive

--- app/layout/actions/toggleMenu.js
+++ app/layout/actions/toggleMenu.js
@@ -3,7 +3,7 @@
 
     angular.module('app.layout').directive('toggleMenu', function(){
         return {
-            restrict: 'A',
+            restrict: 'E',
             link: function(scope, element){
                 var $body = $('body');
 
