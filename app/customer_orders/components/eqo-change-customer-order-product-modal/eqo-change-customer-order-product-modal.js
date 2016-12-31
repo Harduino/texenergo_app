e69994f5562208b7499db79224b08b0f8db01e7e
Alex Elgin <sascha.elgin@gmail.com>
2016-12-31 07:03:51 +0600

    use ES6 in the 'Eqo Change Customer Order Product' modal controller

--- app/customer_orders/components/eqo-change-customer-order-product-modal/eqo-change-customer-order-product-modal.js
+++ app/customer_orders/components/eqo-change-customer-order-product-modal/eqo-change-customer-order-product-modal.js
@@ -1,19 +1,21 @@
-'use strict';
-
-angular.module('app.customer_orders')
-    .controller('EqoChangeCustomerOrderProductModalCtrl', ['$uibModalInstance', 'serverApi', 'product', 'config', function($uibModalInstance, serverApi, product, config){
-        var self = this;
-
+class EqoChangeCustomerOrderProductModalCtrl {
+    constructor($uibModalInstance, serverApi, product, config) {
+        this.modalInstance = $uibModalInstance;
         this.pSelectConfig = {startPage: 0, dataMethod: serverApi.getSearch};
         this.data = {selectedProduct: product, productsList: []};
         this.config = angular.extend({title: 'Изменить товар', btnOkText: 'Изменить', btnCancelText: 'Отмена'}, config);
-    
-        this.ok = function () {
-            $uibModalInstance.close(self.data.selectedProduct);
-        };
-    
-        this.cancel = function () {
-            $uibModalInstance.dismiss('cancel');
-        };
-    }])
-;
+    }
+
+    ok () {
+        this.modalInstance.close(this.data.selectedProduct);
+    }
+
+    cancel () {
+        this.modalInstance.dismiss('cancel');
+    }
+}
+
+EqoChangeCustomerOrderProductModalCtrl.$inject = ['$uibModalInstance', 'serverApi', 'product', 'config'];
+
+angular.module('app.customer_orders')
+    .controller('EqoChangeCustomerOrderProductModalCtrl', EqoChangeCustomerOrderProductModalCtrl);
