a78ff9aa3447809bc3f4dd43c33d16616c3ad69a
Alex Elgin <sascha.elgin@gmail.com>
2017-01-31 09:17:26 +0600

    use ES6 in the 'Product to Customer Order' modal controller

--- app/layout/controllers/productToCustomerOrderModalCtrl.js
+++ app/layout/controllers/productToCustomerOrderModalCtrl.js
@@ -1,38 +1,43 @@
-/**
- * Created by Egor Lobanov on 24.03.16.
- */
-(function(){
-    angular.module('app.layout').controller('productToCustomerOrderModalCtrl', ['product', '$uibModalInstance', 'serverApi', 'funcFactory', function(product, $uibModalInstance, serverApi, funcFactory){
-        var self = this;
+class ProductToCustomerOrderModalCtrl {
+    constructor(product, $uibModalInstance, serverApi, funcFactory) {
+        let self = this;
         this.data = {product: product, quantity: null, customerOrdersList: []};
 
-        serverApi.getCustomerOrders(0, null, {}, function(result) {
-            self.data.customerOrdersList = result.data.filter(function(obj) {
+        this.$uibModalInstance = $uibModalInstance;
+        this.serverApi = serverApi;
+        this.funcFactory = funcFactory;
+
+        serverApi.getCustomerOrders(0, null, {}, result => {
+            self.data.customerOrdersList = result.data.filter(obj => {
                 return obj.can_edit;
             });
         });
+    }
 
-        this.cancel = function () {
-            $uibModalInstance.dismiss('cancel');
-        };
+    cancel () {
+        this.$uibModalInstance.dismiss('cancel');
+    }
 
-        this.addProductToCustomerOrder = function(customer_order){
-            var post = {product_id: self.data.product.id, quantity: self.data.quantity || 1};
+    addProductToCustomerOrder (customer_order) {
+        let self = this;
+        let post = {product_id: self.data.product.id, quantity: self.data.quantity || 1};
 
-            serverApi.addCustomerOrderProduct(customer_order.id, post, function(result) {
-                if(result.data.errors) {
-                    funcFactory.showNotification('Не удалось добавить продукт', result.data.errors);
-                } else {
-                    for(var i = 0; i < self.data.customerOrdersList.length; i++) {
-                        if(self.data.customerOrdersList[i].number === customer_order.number) {
-                            self.data.customerOrdersList.splice(i, 1);
-                            break;
-                        }
+        this.serverApi.addCustomerOrderProduct(customer_order.id, post, result => {
+            if(result.data.errors) {
+                self.funcFactory.showNotification('Не удалось добавить продукт', result.data.errors);
+            } else {
+                for(let i = 0; i < self.data.customerOrdersList.length; i++) {
+                    if(self.data.customerOrdersList[i].number === customer_order.number) {
+                        self.data.customerOrdersList.splice(i, 1);
+                        break;
                     }
-
-                    funcFactory.showNotification('Успешно добавлен в заказ', customer_order.number, true);
                 }
-            });
-        }
-    }]);
-}());
+
+                self.funcFactory.showNotification('Успешно добавлен в заказ', customer_order.number, true);
+            }
+        });
+    }
+}
+
+ProductToCustomerOrderModalCtrl.$inject = ['product', '$uibModalInstance', 'serverApi', 'funcFactory'];
+angular.module('app.layout').controller('productToCustomerOrderModalCtrl', ProductToCustomerOrderModalCtrl);
