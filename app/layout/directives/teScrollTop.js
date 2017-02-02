2007b58833fd17cedb70b35db73fb70aa8eee550
Alex Elgin <sascha.elgin@gmail.com>
2017-02-02 14:53:13 +0600

    refactor the `te-scroll-top` directive

--- app/layout/directives/teScrollTop.js
+++ app/layout/directives/teScrollTop.js
@@ -3,32 +3,19 @@
  * Append button to scroll top
  */
 (function(){
-    angular.module('app.layout').directive('teScrollTop', ['$parse', function($parse){
+    angular.module('app.layout').directive('teScrollTop', ['$parse', function($parse) {
         return {
             restrict: 'E',
-            scope:{},
-            link: function(scope, element, attrs){
-                var config = angular.extend({container: window, buttonClass: ''}, $parse(attrs.config || {})());
-                element.addClass("scroll-up-btn " + config.buttonClass + ' not-selectable');
-                var w = $(config.container);
+            scope: {},
+            link: function($scope, element, attrs) {
+                let config = angular.extend({container: window, buttonClass: ''}, $parse(attrs.config || {})());
+                $scope.buttonClass = config.buttonClass;
+                let block = $(config.container);
 
-                var onScroll = function () {
-                    if (w.scrollTop() >= 2 * w.outerHeight()) {
-                        !element.hasClass('slideUp') && element.addClass('slideUp');
-                    } else {
-                        element.removeClass('slideUp');
-                    }
-                };
-
-                w.scroll(onScroll);
-
-                scope.teScrollTop = function() {
-                    w.scrollTop(0);
-                };
-
-                scope.$on('$destroy', function(){
-                   w.off('scroll', onScroll);
-                });
+                let onScroll = () => $scope.slideUp = block.scrollTop() >= 2 * block.outerHeight();
+                block.scroll(onScroll);
+                $scope.teScrollTop = () => block.scrollTop(0);
+                $scope.$on('$destroy', () => block.off('scroll', onScroll));
             },
             templateUrl: 'app/layout/components/te-scroll-top/te-scroll-top.html'
         };
