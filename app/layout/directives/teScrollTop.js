cad96300f9fe8d844d73d6ed9ad7ce9998a5f6c4
Alex Elgin <sascha.elgin@gmail.com>
2017-02-02 15:27:08 +0600

    extract the controller class in the `te-scroll-top` directive

--- app/layout/directives/teScrollTop.js
+++ app/layout/directives/teScrollTop.js
@@ -1,21 +1,30 @@
-/**
- * Created by Egor Lobanov on 21.11.15.
- * Append button to scroll top
- */
-(function(){
-    angular.module('app.layout').directive('teScrollTop', function() {
-        return {
-            restrict: 'E',
-            scope: {selector: '@', buttonClass: '@'},
-            controller: function($scope) {
-                let block = $($scope.selector || window);
-                let onScroll = () => $scope.slideUp = block.scrollTop() >= 2 * block.outerHeight();
+class ScrollTopCtrl {
+    constructor($scope) {
+        this.block = $($scope.selector || window);
+        this.slideUp = false;
+        this.block.scroll(this.onScroll.bind(this));
 
-                block.scroll(onScroll);
-                $scope.teScrollTop = () => block.scrollTop(0);
-                $scope.$on('$destroy', () => block.off('scroll', onScroll));
-            },
-            templateUrl: 'app/layout/components/te-scroll-top/te-scroll-top.html'
-        };
-    });
-}());
+        let self = this;
+        $scope.$on('$destroy', () => self.block.off('scroll', self.onScroll));
+    }
+
+    teScrollTop () {
+        this.block.scrollTop(0);
+    }
+
+    onScroll () {
+        this.slideUp = this.block.scrollTop() >= 2 * this.block.outerHeight();
+    }
+}
+
+ScrollTopCtrl.$inject = ['$scope'];
+
+angular.module('app.layout').directive('teScrollTop', function() {
+    return {
+        restrict: 'E',
+        scope: {selector: '@', buttonClass: '@'},
+        controller: ScrollTopCtrl,
+        controllerAs: '$ctrl',
+        templateUrl: 'app/layout/components/te-scroll-top/te-scroll-top.html'
+    };
+});
