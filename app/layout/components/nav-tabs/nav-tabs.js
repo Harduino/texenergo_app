4e8cd184e37018eefe95b806fac9432549b4e548
Alex Elgin <sascha.elgin@gmail.com>
2017-02-03 16:06:58 +0600

    extract the controller class from the `nav-tabs` component

--- app/layout/components/nav-tabs/nav-tabs.js
+++ app/layout/components/nav-tabs/nav-tabs.js
@@ -1,8 +1,14 @@
-angular.module('app.layout').component('navTabs', {
-    controller: function($element) {
+class NavTabsCtrl {
+    constructor($element) {
         $element.find('a[data-toggle="tab"]').each((ind, link) => {
-            var l = $(link);
+            let l = $(link);
             l.attr('data-target', l.attr('href')).removeAttr('href');
         });
     }
+}
+
+NavTabsCtrl.$inject = ['$element'];
+
+angular.module('app.layout').component('navTabs', {
+    controller: NavTabsCtrl
 });
