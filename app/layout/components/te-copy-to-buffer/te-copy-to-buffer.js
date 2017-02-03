266221afa684b95b9aa69bc5d41e07b26c335017
Alex Elgin <sascha.elgin@gmail.com>
2017-02-03 14:10:42 +0600

    refactor the `copy-to-buffer` component

--- app/layout/components/te-copy-to-buffer/te-copy-to-buffer.js
+++ app/layout/components/te-copy-to-buffer/te-copy-to-buffer.js
@@ -1,38 +1,28 @@
 class CopyToBufferCtrl {
     constructor($timeout, $element) {
-        let self = this;
         this.$timeout = $timeout;
         this.$element = $element;
 
         this.input = $element.find('.te-c-buffer-input');
-        this.btn = $element.find('.te-c-to-buffer-ico');
         this.showTooltip = false;
-
-        this.btn.click(this.copy.bind(this));
-        this.$onDestroy = () => self.btn.off('click', this.copy.bind(this));
     }
 
     copy () {
-        let value = '', values = this.$element.find('[te-buffer-value=""]');
+        let copiedText = '', values = this.$element.find('[te-buffer-value=""]');
         let self = this;
 
         values.each((index, item) => {
             let separator = index !== (values.length - 1) ? ' | ' : '';
-            value += $(item)[['INPUT', 'TEXTAREA'].indexOf(item.nodeName) === -1 ? 'html' : 'val']() + separator;
+            copiedText += $(item)[['INPUT', 'TEXTAREA'].indexOf(item.nodeName) === -1 ? 'html' : 'val']() + separator;
         });
 
-        this.input.val(value);
+        this.input.val(copiedText);
         this.input.focus();
         this.input.select();
 
         document.execCommand('copy');
-        this.btn.addClass('animate');
         this.showTooltip = true;
-
-        this.$timeout(() => {
-            self.btn.removeClass('animate');
-            self.showTooltip = false;
-        }, 820);
+        this.$timeout(() => self.showTooltip = false, 820);
     }
 }
 
