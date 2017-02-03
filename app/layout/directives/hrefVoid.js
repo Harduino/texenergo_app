79528cb9b26a80c6e084132b2435d7e3324f9ebb
Alex Elgin <sascha.elgin@gmail.com>
2017-02-03 15:53:56 +0600

    use element selector in the `nav-tabs` directive

--- app/layout/directives/hrefVoid.js
+++ app/layout/directives/hrefVoid.js
@@ -5,7 +5,7 @@
     angular.module('app.layout').directive('hrefVoid', function () {
         return {
             restrict: 'A',
-            link: function (scope, element, attributes) {
+            link: function (scope, element) {
                 element.attr('href','#');
                 element.on('click', function(e){
                     e.preventDefault();
@@ -16,8 +16,8 @@
     })
     .directive('navTabs', function(){
         return {
-            restrict: 'C',
-            link: function(scope, element){
+            restrict: 'E',
+            link: function(scope, element) {
                 element.find('a[data-toggle="tab"]').each(function(ind, link){
                     var l = $(link);
                     l.attr('data-target', l.attr('href')).removeAttr('href');
