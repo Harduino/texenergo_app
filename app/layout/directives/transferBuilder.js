1cf725ade2d4f90bb9f9313093c1bf33feb0b712
Alex Elgin <sascha.elgin@gmail.com>
2017-02-06 17:57:48 +0600

    refactor the `transfer-builder` directive

--- app/layout/directives/transferBuilder.js
+++ app/layout/directives/transferBuilder.js
@@ -3,63 +3,55 @@
  * directive of create transfer form
  */
 (function(){
-    angular.module('app.layout').directive('transferBuilder', ['serverApi', 'funcFactory', '$filter', function(serverApi, funcFactory, $filter){
+    angular.module('app.layout').directive('transferBuilder', ['serverApi', 'funcFactory', function(serverApi, funcFactory) {
         return {
             restrict: 'E',
-            scope:{
-                config: '=',
-                transfersList: '='
-            },
-            link: function(scope, element, attrs){
-                var _modal = element.find('.modal');
+            scope: {config: '=', transfersList: '='},
+            link: function($scope, $element) {
+                var _modal = $element.find('.modal');
 
-                scope.partnerSelectConfig ={
-                    dataMethod: serverApi.getPartners
-                };
+                $scope.partnerSelectConfig = {dataMethod: serverApi.getPartners};
+                $scope.data = {partnersList: []};
+                $scope.datePickerConfig = {dateFormat: 'dd.mm.yy'};
 
-                scope.data = {
-                    partnersList:[]
-                };
-
-                scope.config.showForm = function(){
+                $scope.config.showForm = function() {
                     clearForm();
                     _modal.modal('show');
                 };
 
-                scope.datePickerConfig = {
-                    dateFormat: 'dd.mm.yy'
-                };
-
                 clearForm();
 
-                scope.createTransfer = function(){
-                    var data = scope.newTransfer;
-                    if(data.partner) data.partner_id = data.partner.id;
+                $scope.createTransfer = function(){
+                    var data = $scope.newTransfer;
+
+                    if(data.partner) {
+                        data.partner_id = data.partner.id;
+                    }
+
                     delete data.partner;
 
-                    var appendTransfer = function(item){
-                        scope.transfersList.unshift(item);
+                    var appendTransfer = function(item) {
+                        $scope.transfersList.unshift(item);
                         funcFactory.showNotification((item.type === 'IncomingTransfer' ? 'Входящий' : 'Исходящий') + ' платеж успешно добавлен', item.number, true);
                     };
 
-                    scope.config.createMethod(data, function(result){
-                        if(!result.data.errors){
-                            angular.isArray(result.data) ? result.data.map(function(item){
-                                appendTransfer(item);
-                            }) : appendTransfer(result.data);
-
+                    $scope.config.createMethod(data, function(result) {
+                        if(!result.data.errors) {
+                            angular.isArray(result.data) ? result.data.map(appendTransfer) : appendTransfer(result.data);
                             _modal.modal('hide');
                             clearForm();
-                        }else funcFactory.showNotification('Не удалось создать платеж', result.data.errors);
+                        } else {
+                            funcFactory.showNotification('Не удалось создать платеж', result.data.errors);
+                        }
                     }, angular.noop);
                 };
 
-                function clearForm(){
-                    scope.newTransfer = {
+                function clearForm() {
+                    $scope.newTransfer = {
                         date: new Date(),
-                        amount:0,
-                        partner:{},
-                        partner_id:'',
+                        amount: 0,
+                        partner: {},
+                        partner_id: '',
                         number: null,
                         description: ''
                     };
