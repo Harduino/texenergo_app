2afc0cd6aeb3ec5338bccf3774f2bf941f9258bb
Alex Elgin <sascha.elgin@gmail.com>
2017-02-08 15:53:34 +0600

    extract a view file from the `te-infnity-scroll` directive

--- app/layout/directives/te-inifnity-scroll.js
+++ app/layout/directives/te-inifnity-scroll.js
@@ -4,46 +4,38 @@
 (function(){
     "use strict";
 
-    angular.module('te.infinity.scroll', []).directive('teInfinity', ['$q', '$timeout', '$compile', function($q, $timeout, $compile) {
+    angular.module('te.infinity.scroll', []).directive('teInfinityScroll', ['$q', '$timeout', function($q, $timeout) {
         return {
             restrict: 'E',
-            scope: {config: '=', selector: '@'},
-            link: function(scope, element){
+            scope: {config: '=', selector: '@', view: '@'},
+            link: function($scope, $element) {
                 var config = {
-                    startFrom:0,
-                    startPage:1,
-                    scrollDistance:30,
+                    startFrom: 0,
+                    startPage: 1,
+                    scrollDistance: 30,
                     delay: 500,
                     dataMethod: angular.noop,
                     liveLoad: false,
                     loadAfterInit: true,
-                    resultCollection: [],
-                    searchStatusTmpl: '<div class="ui-select-status">' +
-                        '<span ng-if="searchStatus == \'before\'">Введите еще хотя бы {{config.startFrom}} символа</span>'+
-                        '<span ng-if="searchStatus == \'noresult\'">Нет результатов</span>'+
-                        '<span ng-if="searchStatus == \'inload\'">Загрузка...</span>'+
-                        '</div>'
+                    resultCollection: []
                 };
 
-                var block = $(scope.selector);
+                var block = $($scope.selector);
 
                 var page,                                       // current page for load
-                    content = $(element[0].firstElementChild),  // content of scroll
+                    content,                                    // content of scroll
                     inLoad = false,                             // loading status
                     query,                                      // search query
                     canceler,                                   // request canceler
                     timeout_p,                                  // $timeout promise
                     elHeight = block.outerHeight();
 
-                config = angular.extend(config, scope.config);
-
-                element.append($compile(config.searchStatusTmpl)(scope));
-
+                config = angular.extend(config, $scope.config);
                 block.scroll(scroll);
 
-                config.liveLoad ? scope.$watch('config.queryModel', function(value){
+                config.liveLoad ? $scope.$watch('config.queryModel', function(value){
                     setQueryValue(value);
-                }) : scope.config.setQueryValue = setQueryValue;
+                }) : $scope.config.setQueryValue = setQueryValue;
 
                 config.loadAfterInit && setQueryValue("");
 
@@ -54,15 +46,24 @@
                         page = config.startPage;
                         inLoad = false;
                         query = value;
-                        scope.config.resultCollection = [];
-                        if(value.length>=config.startFrom) timeout_p = $timeout(load, config.delay);
-                        else scope.searchStatus='before';
+                        $scope.config.resultCollection = [];
+
+                        if(value.length >= config.startFrom) {
+                            timeout_p = $timeout(load, config.delay);
+                        } else {
+                            $scope.searchStatus = 'before';
+                        }
                     }
                 }
 
-                function scroll(){
+                function scroll() {
+                    if(content === undefined) {
+                        content = $($element.find('.te-infinity-scroll-content')[0].firstChild);
+                    }
+
                     var p = content.outerHeight() - block.scrollTop() - elHeight;
-                    if(!inLoad && p<config.scrollDistance && p>-elHeight/2){
+
+                    if(!inLoad && p < config.scrollDistance && p > -elHeight / 2) {
                         page++;
                         load(config.notShowLoadStatus);
                     }
@@ -70,28 +71,29 @@
 
                 function load (notShowStatus){
                     inLoad = true;
-                    scope.searchStatus= notShowStatus ? 'result' : 'inload';
+                    $scope.searchStatus= notShowStatus ? 'result' : 'inload';
                     canceler = $q.defer();
                     config.dataMethod(page, query, {timeout:canceler.promise}, function(result){
                         if(result.status == 200){
-                            inLoad = result.data.length==0;
-                            //scope.resultCollection =  scope.$select.items.concat(result.data);
+                            inLoad = result.data.length == 0;
+
                             result.data.hasOwnProperty('length') && result.data.map(function(item){
-                                scope.config.resultCollection.push(item);
+                                $scope.config.resultCollection.push(item);
                             });
-                            if(page == config.startPage && inLoad) scope.searchStatus='noresult';
-                            else scope.searchStatus='result';
-                        }else {
+
+                            $scope.searchStatus = (page == config.startPage) && inLoad ? 'noresult' : 'result';
+                        } else {
                             inLoad = false;
-                            scope.searchStatus='noresult';
+                            $scope.searchStatus='noresult';
                         }
                     });
                 }
 
-                scope.$on('$destroy', function(){
+                $scope.$on('$destroy', function(){
                     block.off('scroll');
                 });
-            }
-        }
+            },
+            templateUrl: 'app/layout/components/te-infinity-scroll/te-infinity-scroll.html'
+        };
     }]);
 }());
