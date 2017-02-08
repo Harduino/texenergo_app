fb7eed9503341561709ccf854ebe76ba53cf9839
Alex Elgin <sascha.elgin@gmail.com>
2017-02-08 18:27:05 +0600

    add live-reload option to the `te-list-filter` component

--- app/layout/components/te-list-filter/te-list-filter.js
+++ app/layout/components/te-list-filter/te-list-filter.js
@@ -1,20 +1,35 @@
 class ListFilterCtrl {
-    constructor(Observer) {
+    constructor($timeout, Observer) {
+        this.$timeout = $timeout;
         this.Observer = Observer;
+
+        if(this.delay === undefined) {
+            this.delay = 500;
+        }
     }
 
-    filterList(e) {
-        if((e.keyCode == 13) || (e.type == 'click')) {
-            let self = this;
-            this.Observer.notify('FILTER_LIST', {listId: self.listId, filter: self.model});
+    filterList (e, runTimer) {
+        if(this.timerId !== undefined) {
+            this.$timeout.cancel(this.timerId);
         }
+
+        if((e.keyCode == 13) || (e.type == 'click')) {
+            this.applyFilter();
+        } else if(runTimer && this.liveLoad) {
+            this.timerId = this.$timeout(this.applyFilter.bind(this), this.delay);
+        }
+    }
+
+    applyFilter () {
+        let self = this;
+        this.Observer.notify('FILTER_LIST', {listId: self.listId, filter: self.model});
     }
 }
 
-ListFilterCtrl.$inject = ['Observer'];
+ListFilterCtrl.$inject = ['$timeout', 'Observer'];
 
 angular.module('app.layout').component('teListFilter', {
-    bindings: {listId: '@'},
+    bindings: {delay: '@', listId: '@', liveLoad: '<'},
     controller: ListFilterCtrl,
     controllerAs: '$ctrl',
     templateUrl: 'app/layout/components/te-list-filter/te-list-filter.html'
