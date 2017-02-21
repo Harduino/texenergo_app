565971f2092978b6f9dd508cea6eefe83220d2eb
Alex Elgin <sascha.elgin@gmail.com>
2017-02-21 15:07:21 +0600

    clean up the `replacement-product-modal` component

--- app/products/components/replacement-product-modal/replacement-product-modal.js
+++ app/products/components/replacement-product-modal/replacement-product-modal.js
@@ -1,27 +1,22 @@
 class ReplacementProductModalCtrl{
-    constructor(serverApi){
-        let self = this;
-        this.serverApi = serverApi;
-
-        this.pSelectConfig = {
-            startPage: 0,
-            dataMethod: self.serverApi.getSearch
-        };
+    constructor(serverApi) {
+        this.pSelectConfig = {startPage: 0, dataMethod: serverApi.getSearch};
         this.selectedProduct = {};
-        this.productsList = [];
     }
+
     ok () {
         this.modalInstance.close();
     }
+
     cancel () {
         this.modalInstance.dismiss('cancel');
     }
-    
 }
 
 ReplacementProductModalCtrl.$inject = ['serverApi'];
+
 angular.module('app.products').component('replacementProductModal', {
     controller: ReplacementProductModalCtrl,
-    controllerAs: 'replacementProductModalCtrl',
-    bindings: { modalInstance: "<", resolve: "<" }
+    controllerAs: '$ctrl',
+    bindings: {modalInstance: '<', resolve: '<'}
 });
