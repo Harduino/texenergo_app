b74671d60bee8beea4eb92c36e07df8f1192d8c1
Alex Elgin <sascha.elgin@gmail.com>
2017-03-11 17:39:52 +0600

    use ES6 in the `form-nav-buttons` component

--- app/layout/components/form-nav-buttons/form-nav-buttons.js
+++ app/layout/components/form-nav-buttons/form-nav-buttons.js
@@ -1,84 +1,78 @@
-/**
- * Created by Egor Lobanov on 08.11.15.
- * Директива создающая кнопки навигации на основе прав пользователя и наличия тех или иных кнопок на странице
- */
-(function(){
-    'use strict';
+class FormNavButtonsCtrl {
+    constructor() {
+        this.showText = (this.template === undefined) || (this.template !== 'table');
 
-    angular.module('app.layout').component('formNavButtons', {
-        bindings: {
-            role: '=',
-            options:'=',//список кнопок которые необходимо отобразить
-            subdata: '=', // модель или данные которые необходимо вернуть в хендлер клика
-            contentClass: '@',
-            template: '@'
-        },
-        controller: function() {
-            var self = this;
-            var roles = this.role || {};
-            this.showText = (this.template === undefined) || (this.template !== 'table');
+        const AVAILABLE_BUTTONS = {
+            new:{name: 'Новый', ico: 'plus'},
+            show: {name:'Смотреть', ico:'search'},
+            edit:{name:'Изменить',ico:'pencil'},
+            files:{name:'Файлы', ico:'file'},
+            recalculate:{name:'Пересчитать', ico:'repeat', role:'can_edit'},
+            send_email:{name:'Отправить', ico:'envelope'},
+            logs:{name:'История', ico:'book'},
+            confirm_order:{ico:'check'/*, role:'can_confirm'*/},
+            view: {ico:'eye', class:'btn-info', disabled:false},
+            remove:{ico:'trash-o', class: 'btn-danger', role:'can_destroy', disabled:true},
+            table_edit:{ico:'pencil', class:'btn-default', role:'can_edit', disabled:true},
+            back:{name:'К списку', ico:'arrow-left'},
+            upd_form_pdf:{name:'УПД', ico:'file-pdf-o'},
+            txt_export: {name: 'Текст', ico: 'file-text'},
+            label_pdf:{name:'Бирка', ico:'truck'},
+            packing_list_pdf:{name:'Упаковочный лист', ico:'list-ol'},
+            at_partners:{name:'У партнёров', ico:'exchange'},
+            automatically: {name: 'Автоматически', ico: 'rocket'},
+            add: {ico: 'plus', class: 'btn-success', name: 'В заказ'},
+            refresh: {ico: 'refresh', name: 'Обновить'},
+            command: {ico: 'keyboard-o', name: 'Комманды'}
+        };
 
-            var AVAILABLE_BUTTONS = {
-                new:{name: 'Новый', ico: 'plus'},
-                show: {name:'Смотреть', ico:'search'},
-                edit:{name:'Изменить',ico:'pencil'},
-                files:{name:'Файлы', ico:'file'},
-                recalculate:{name:'Пересчитать', ico:'repeat', role:'can_edit'},
-                send_email:{name:'Отправить', ico:'envelope'},
-                logs:{name:'История', ico:'book'},
-                confirm_order:{ico:'check'/*, role:'can_confirm'*/},
-                view: {ico:'eye', class:'btn-info', disabled:false},
-                remove:{ico:'trash-o', class: 'btn-danger', role:'can_destroy', disabled:true},
-                table_edit:{ico:'pencil', class:'btn-default', role:'can_edit', disabled:true},
-                back:{name:'К списку', ico:'arrow-left'},
-                upd_form_pdf:{name:'УПД', ico:'file-pdf-o'},
-                txt_export: {name: "Текст", ico: 'file-text'},
-                label_pdf:{name:'Бирка', ico:'truck'},
-                packing_list_pdf:{name:'Упаковочный лист', ico:'list-ol'},
-                at_partners:{name:'У партнёров', ico:'exchange'},
-                automatically: {name: 'Автоматически', ico: 'rocket'},
-                add: {ico: "plus", class: 'btn-success', name: "В заказ"},
-                refresh: {ico: "refresh", name: "Обновить"},
-                command: {ico: "keyboard-o", name: "Комманды"}
-            };
+        let self = this;
+        let roles = this.role || {};
+        this.buttons = [];
+        //пробегаемся по списку кнопок и проверяем существуют ли настроки для кнопки
+        if (angular.isArray(this.options) && this.options.length > 0) {
+            this.options.map(item => {
+                let button = AVAILABLE_BUTTONS[item.type], role = button.role;
 
-            this.buttons = [];
-            //пробегаемся по списку кнопок и проверяем существуют ли настроки для кнопки
-            if (angular.isArray(this.options) && this.options.length > 0) {
-                this.options.map(function(item) {
-                    var button = AVAILABLE_BUTTONS[item.type], role = button.role;
+                if (button) {
+                    button.callback = item.callback;
+                    button.class = (self.contentClass || 'btn btn-success') + ' ' + (button.class || '');
 
-                    if (button) {
-                        button.callback = item.callback;
-                        button.class = (self.contentClass || 'btn btn-success') + ' ' + (button.class || '');
-
-                        if(button.hasOwnProperty('disabled') && role && roles[role]) {
-                            button.disabled = !roles[role];
-                        }
-
-                        if(item.type === 'confirm_order') {
-                            createConfirmOrderControls(self.subdata, button, self.buttons);
-                            return 0;
-                        }
-
-                        self.buttons.push(button);
+                    if(button.hasOwnProperty('disabled') && role && roles[role]) {
+                        button.disabled = !roles[role];
                     }
-                });
-            }
 
-            var createConfirmOrderControls = function (data, button, btnList) {
-                if(data && data.events && angular.isArray(data.events)) {
-                    data.events.map(function(event) {
-                        btnList.push(angular.extend(event, button));
-                    });
+                    if(item.type === 'confirm_order') {
+                        return self.createConfirmOrderControls(button);
+                    }
+
+                    self.buttons.push(button);
                 }
-            };
+            });
+        }
+    }
 
-            this.handleClick = function (button) {
-                !button.disabled && button.callback && button.callback(self.subdata, button);
-            };
-        },
-        controllerAs: '$ctrl',
-        templateUrl: 'app/layout/components/form-nav-buttons/form-nav-buttons.html'
-    });
-}());
+    handleClick (button) {
+        !button.disabled && button.callback && button.callback(this.subdata, button);
+    }
+
+    createConfirmOrderControls (button) {
+        if(this.subdata && this.subdata.events && angular.isArray(this.subdata.events)) {
+            let self = this;
+            self.subdata.events.map(event => self.buttons.push(angular.extend(event, button)));
+        }
+    }
+}
+
+angular.module('app.layout').component('formNavButtons', {
+    bindings: {
+        role: '=',
+        options:'=',//список кнопок которые необходимо отобразить
+        subdata: '=', // модель или данные которые необходимо вернуть в хендлер клика
+        contentClass: '@',
+        template: '@'
+    },
+    controller: FormNavButtonsCtrl,
+    controllerAs: '$ctrl',
+    templateUrl: 'app/layout/components/form-nav-buttons/form-nav-buttons.html'
+});
