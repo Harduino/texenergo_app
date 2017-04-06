5aca84081592e5115d9f10f5a108aa17423cc9ba
Alex Elgin <sascha.elgin@gmail.com>
2017-04-06 14:46:32 +0600

    simpify the `toggle-shortcut` directive

--- app/components/shortcut/shortcut-directive.js
+++ app/components/shortcut/shortcut-directive.js
@@ -1,66 +1,38 @@
-/**
- * Shortcut-directive
- */
 class ToggleShortcut{
-    constructor($timeout, authService, $compile, $state){
-
+    constructor($timeout, authService, $compile, $state) {
         let initDomEvents = function ($element, $scope) {
+            var shortcutBlock = $('#shortcut');
+            $compile(shortcutBlock)($scope);
 
-            var shortcut_dropdown = $('#shortcut');
-
-            $compile(shortcut_dropdown)($scope);
-
-            $scope.goToShortCutItem = function(state, params){
-                var p = params || null;
-
-                if(state === 'app.contacts.view'){
-                    var authProfile = authService.profile;
-                    if(authProfile){
-                        p = {
-                            id:authProfile.user_metadata.contact_id
-                        };
-                    }
-                }
-
-                $state.go(state, p);
-                window.setTimeout(shortcut_buttons_hide, 300);
+            $scope.navigateToProfilePage = function() {
+                var authProfile = authService.profile;
+                $state.go('app.contacts.view', authProfile ? {id: authProfile.user_metadata.contact_id} : null);
+                $timeout(hideShortcutBlock, 300);
             };
 
-            $element.on('click', function () {
-                if (shortcut_dropdown.is(":visible")) {
-                    shortcut_buttons_hide();
-                } else {
-                    shortcut_buttons_show();
-                }
-
-            });
+            $element.on('click', showShortcutBlock);
 
             // SHORTCUT buttons goes away if mouse is clicked outside of the area
             $(document).mouseup(function (e) {
-                if (shortcut_dropdown && !shortcut_dropdown.is(e.target) && shortcut_dropdown.has(e.target).length === 0) {
-                    shortcut_buttons_hide();
+                if (shortcutBlock && !shortcutBlock.is(e.target) && shortcutBlock.has(e.target).length === 0) {
+                    hideShortcutBlock();
                 }
             });
 
             // SHORTCUT ANIMATE HIDE
-            function shortcut_buttons_hide() {
-                shortcut_dropdown.animate({
-                    height: "hide"
-                }, 300, "easeOutCirc");
+            function hideShortcutBlock() {
+                shortcutBlock.animate({height: 'hide'}, 300, 'easeOutCirc');
                 $('body').removeClass('shortcut-on');
-
             }
 
             // SHORTCUT ANIMATE SHOW
-            function shortcut_buttons_show() {
-                shortcut_dropdown.animate({
-                    height: "show"
-                }, 200, "easeOutCirc");
+            function showShortcutBlock() {
+                shortcutBlock.animate({height: 'show'}, 200, 'easeOutCirc');
                 $('body').addClass('shortcut-on');
             }
         };
 
-        let link = function($scope, $element){
+        let link = function($scope, $element) {
             $timeout(function(){
                 initDomEvents($element, $scope);
             });
@@ -71,7 +43,7 @@ class ToggleShortcut{
     }
 }
 
-toggleShortcut.$inject = ['$timeout', 'authService', '$compile', '$state'];
+ToggleShortcut.$inject = ['$timeout', 'authService', '$compile', '$state'];
 
 function toggleShortcut($timeout, authService, $compile, $state){
     return new ToggleShortcut($timeout, authService, $compile, $state);
