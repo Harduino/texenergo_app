ade11c1402a7673cf56a718c8c1eec9a0583b29e
Egor Lobanov <trickster13r@icloud.com>
2017-04-28 03:53:39 +0300

    toggle button

---
+++ app/layout/components/te-switcher/te-switcher.js
@@ -0,0 +1,35 @@
+// Component creates toggle button
+// Usage: <te-switcher ng-model="selected"></te-switcher>
+class TeSwitcher{
+  constructor(){
+    console.log(this.model);
+  }
+
+  onClick($event){
+    $event.preventDefault();
+    $event.stopImmediatePropagation();
+
+    var model = this.model;
+
+    model.$setViewValue(!model.$viewValue);
+    model.$render();
+  }
+}
+
+angular.module('app.layout').component('teSwitcher', {
+  controllerAs: '$teSwitcher',
+  controller: TeSwitcher,
+  // bindings: {model: '='},
+  require: {
+    model:'^ngModel'
+  },
+  template: `
+    <span class="onoffswitch" ng-click="$teSwitcher.onClick($event)">
+      <input type="checkbox" ng-model="$teSwitcher.model.$modelValue" class="onoffswitch-checkbox">
+      <label class="onoffswitch-label">
+        <span class="onoffswitch-inner"></span>
+        <span class="onoffswitch-switch"></span>
+      </label>
+    </span>
+  `
+});
