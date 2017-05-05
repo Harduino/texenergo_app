25ef14516d30359d57582bc33affa8d17d1c63a0
Alex Elgin <sascha.elgin@gmail.com>
2017-05-05 10:31:01 +0600

    fix the `lazy-loaded-list` component

--- app/layout/components/lazy-loaded-list/lazy-loaded-list.js
+++ app/layout/components/lazy-loaded-list/lazy-loaded-list.js
@@ -1,47 +1,13 @@
 class LazyLoadedListCtrl {
     constructor (serverApi) {
-        this.startPage = (this.startPage === undefined) || (this.startPage === null) ? 1 : parseInt(this.startPage);
-        this.pageNumber = this.startPage;
-
-        this.disabled = false;
-        this.notFound = false;
-        this.showLoader = false;
-
-        if(this.apiMethod !== undefined) {
-            this.fetch = serverApi[this.apiMethod];
-        }
-
-        this.loadItems = this.loadItems.bind(this);
-        this.loadItems();
-    }
-
-    loadItems () {
-        if(this.disabled || !this.fetch) {
-            return;
-        }
-
-        this.disabled = true;
-        this.showLoader = true;
-        let self = this;
-
-        this.fetch(this.pageNumber, this.searchQuery, {}, res => {
-            self.showLoader = false;
-            self.disabled = res.data.length === 0;
-
-            if(self.disabled) {
-                self.notFound = self.pageNumber === self.startPage;
-            } else {
-                self.items = self.items.concat(res.data);
-                self.pageNumber++;
-            }
-        });
+        this.fetch = serverApi[this.apiMethod];
     }
 }
 
 LazyLoadedListCtrl.$inject = ['serverApi'];
 
 angular.module('app.layout').component('lazyLoadedList', {
-    bindings: {apiMethod: '@', fetch: '=', items: '=', navOptions: '=', role: '=', searchQuery: '=', startPage: '@', view: '@'},
+    bindings: {apiMethod: '@', items: '=', navOptions: '=', role: '=', searchQuery: '=', view: '@'},
     templateUrl: 'app/layout/components/lazy-loaded-list/lazy-loaded-list.html',
     controller: LazyLoadedListCtrl,
     controllerAs: '$ctrl'
---
+++ app/layout/components/raw-lazy-loaded-list/raw-lazy-loaded-list.js
@@ -0,0 +1,42 @@
+class RawLazyLoadedListCtrl {
+    constructor () {
+        this.startPage = (this.startPage === undefined) || (this.startPage === null) ? 1 : parseInt(this.startPage);
+        this.pageNumber = this.startPage;
+
+        this.disabled = false;
+        this.notFound = false;
+        this.showLoader = false;
+
+        this.loadItems = this.loadItems.bind(this);
+        this.loadItems();
+    }
+
+    loadItems () {
+        if(this.disabled || !this.fetch) {
+            return;
+        }
+
+        this.disabled = true;
+        this.showLoader = true;
+        let self = this;
+
+        this.fetch(this.pageNumber, this.searchQuery, {}, res => {
+            self.showLoader = false;
+            self.disabled = res.data.length === 0;
+
+            if(self.disabled) {
+                self.notFound = self.pageNumber === self.startPage;
+            } else {
+                self.items = self.items.concat(res.data);
+                self.pageNumber++;
+            }
+        });
+    }
+}
+
+angular.module('app.layout').component('rawLazyLoadedList', {
+    bindings: {fetch: '=', items: '=', navOptions: '=', role: '=', searchQuery: '=', startPage: '@', view: '@'},
+    templateUrl: 'app/layout/components/raw-lazy-loaded-list/raw-lazy-loaded-list.html',
+    controller: RawLazyLoadedListCtrl,
+    controllerAs: '$ctrl'
+});
