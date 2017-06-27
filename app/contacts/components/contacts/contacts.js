9a9a31f356f1a5718f00030dfa6f48c6a13a68f3
Egor Lobanov <trickster13r@icloud.com>
2017-06-27 17:26:42 +0300

    filling fio of contact  via dadata

--- app/contacts/components/contacts/contacts.js
+++ app/contacts/components/contacts/contacts.js
@@ -27,31 +27,91 @@ class ContactsCtrl {
     }
 
     getSuggestions(type, val, field) {
-        let self = this;
+        let self = this,
+            params = {query: val};
 
-        return this.serverApi.validateViaDaData(type, {query: val}).then(result => {
-            return result.data.suggestions.map(item => {
-                return self.$parse(field)(item) || val
-            });
+        if(type === 'fio'){
+          params = self.getFIOParameters(field, params);
+        }
+
+        return this.serverApi.validateViaDaData(type, params).then(result => {
+          return result.data.suggestions;
         });
     }
 
-    createContact() {
-        if(this.newContact.partner) {
-            this.newContact.partner_id = this.newContact.partner.id;
+    getFIOParameters(field, params){
+      let contact = this.newContact,
+          p = params || {};
+
+      if(field === 'name'){
+        if(angular.isObject(contact.last_name)){
+          p.gender = contact.last_name.data.gender;
+          p.parts = ["NAME"];
         }
+      }else{
+        if(angular.isObject(contact.first_name)){
+          p.gender = contact.first_name.data.gender;
+          p.parts = ["SURNAME"];
+        }
+      }
 
-        delete this.newContact.partner;
-        let self = this;
+      return p;
+    }
 
-        this.serverApi.createContact(self.newContact, res => {
-            if(!res.data.errors) {
-                self.data.contactsList.unshift(res.data);
-                self.funcFactory.showNotification('Успешно', 'Контакт ' +  res.data.email + ' добавлен в список', true);
-            } else {
-                self.funcFactory.showNotification('Не удалось создать новый контакт', res.data.errors);
-            }
-        });
+    parseFioLine($item) {
+      let data = $item.data;
+
+      if(data && data.name && data.surname){
+        this.newContact.first_name = {
+          data: {
+            gender: data.gender
+          },
+          value: data.name
+        };
+        this.newContact.last_name = {
+          data: {
+            gender: data.gender
+          },
+          value: data.surname
+        };
+      }
+    }
+
+    switchFioFields() {
+      let f_name = this.newContact.first_name,
+          l_name = this.newContact.last_name;
+
+      this.newContact.first_name = l_name;
+      this.newContact.last_name = f_name;
+    }
+
+    createContact() {
+      let newContact = this.newContact;
+
+      if(newContact.partner) {
+        newContact.partner_id = newContact.partner.id;
+      }
+      if(angular.isObject(newContact.first_name)){
+        newContact.first_name = newContact.first_name.value;
+      }
+      if(angular.isObject(newContact.last_name)){
+        newContact.last_name = newContact.last_name.value;
+      }
+      if(angular.isObject(newContact.email)){
+        newContact.email = newContact.email.value;
+      }
+
+      delete newContact.partner;
+      let self = this;
+
+      this.serverApi.createContact(newContact, res => {
+          if(!res.data.errors) {
+              self.data.contactsList.unshift(res.data);
+              self.funcFactory.showNotification('Успешно', 'Контакт ' +  res.data.email + ' добавлен в список', true);
+          } else {
+              self.funcFactory.showNotification('Не удалось создать новый контакт', res.data.errors);
+          }
+      });
     }
 }
 
