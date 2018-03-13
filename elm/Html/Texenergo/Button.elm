a987e9bfa64736884a320f52d8dcf78cb31bbb01
Mikhail Arzhaev <marzhaev@texenergo.com>
2018-03-13 17:48:25 +0300

    Fixing compilation error

--- elm/Html/Texenergo/Button.elm
+++ elm/Html/Texenergo/Button.elm
@@ -1,7 +1,18 @@
-module Html.Texenergo.Button exposing (..)
+module Html.Texenergo.Button exposing (button, render, backButton)
 
 import Html exposing (div, text, h1)
 import Html.Attributes exposing (class)
+
+
+backButton : String -> Html.Html msg
+backButton path =
+    Html.a
+        [ class "btn btn-success"
+        , Html.Attributes.href path
+        ]
+        [ Html.i [ class "fa fa-arrow-left" ] []
+        , text "Назад"
+        ]
 
 
 type alias Button =
@@ -21,3 +32,20 @@ render icon title =
             []
         , text title
         ]
+
+
+button : List (Html.Attribute msg) -> List (Html.Html msg) -> String -> String -> Html.Html msg
+button attributes values icon title =
+    div
+        ([ class "btn btn-success"
+         ]
+            ++ attributes
+        )
+        ([ Html.i
+            [ "fa fa-" ++ icon |> class
+            ]
+            []
+         , text title
+         ]
+            ++ values
+        )
